4d3f693d202073d3065d468eead817980dab9a90
Nizamudeen A <nia@redhat.com>
2022-11-30 17:23:01 +0530

    mgr/dashboard: build dashboard for ibm ceph
    I added a new npm command to build both the rhcs and ibm dashboard
    assets from the front-end. This will create two assets folder.
    
    npm run build:all
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>
    (cherry picked from commit c59476581bc934c10516f396b5f4c6a82e00fb91)

--- src/pybind/mgr/dashboard/frontend/src/app/app.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 import { NgbPopoverConfig, NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
+
+import { environment } from '~/environments/environment';
 
 @Component({
   selector: 'cd-root',
@@ -8,11 +11,24 @@ import { NgbPopoverConfig, NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(popoverConfig: NgbPopoverConfig, tooltipConfig: NgbTooltipConfig) {
+  constructor(
+    popoverConfig: NgbPopoverConfig,
+    tooltipConfig: NgbTooltipConfig,
+    @Inject(DOCUMENT) private document: Document
+  ) {
     popoverConfig.autoClose = 'outside';
     popoverConfig.container = 'body';
     popoverConfig.placement = 'bottom';
 
     tooltipConfig.container = 'body';
+
+    if (environment.build === 'ibm') {
+      const headEl = this.document.getElementsByTagName('head')[0];
+      const newLinkEl = this.document.createElement('link');
+      newLinkEl.rel = 'stylesheet';
+      newLinkEl.href = 'ibm-overrides.css';
+
+      headEl.appendChild(newLinkEl);
+    }
   }
 }
