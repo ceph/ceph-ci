942f086d09faa9e91962ac691002dc05867ef1a4
Yehuda Sadeh <ysadehwe@ibm.com>
2023-07-01 09:27:30 -0400

    fscrypt: handle holes
    Signed-off-by: Yehuda Sadeh <ysadehwe@ibm.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -11280,14 +11280,16 @@ int Client::_read_async(Fh *f, uint64_t off, uint64_t len, bufferlist *bl,
                              &fscrypt_denc);
 
   // read (and possibly block)
+  //
   int r = 0;
   if (onfinish == nullptr) {
     io_finish_cond = new C_SaferCond("Client::_read_async flock");
     io_finish.reset(io_finish_cond);
   }
 
-  r = objectcacher->file_read(&in->oset, &in->layout, in->snapid,
-			      read_start, read_len, bl, 0, io_finish.get());
+  std::vector<ObjectCacher::ObjHole> holes;
+  r = objectcacher->file_read_ex(&in->oset, &in->layout, in->snapid,
+                                 read_start, read_len, bl, 0, &holes, io_finish.get());
  
   if (onfinish != nullptr) {
     // put the cap ref since we're releasing C_Read_Async_Finisher
@@ -11315,11 +11317,14 @@ int Client::_read_async(Fh *f, uint64_t off, uint64_t len, bufferlist *bl,
 
   if (r >= 0) {
     if (fscrypt_denc) {
-      r = fscrypt_denc->decrypt_bl(off, target_len, read_start, bl);
+      r = fscrypt_denc->decrypt_bl(off, target_len, read_start, holes, bl);
       if (r < 0) {
         ldout(cct, 20) << __func__ << "(): failed to decrypt buffer: r=" << r << dendl;
+        return r;
       }
     }
+
+    r = bl->length();
 
     update_read_io_size(bl->length());
   } else {
@@ -11416,6 +11421,7 @@ int Client::_read_sync(Fh *f, uint64_t off, uint64_t len, bufferlist *bl,
     bufferlist tbl;
 
     int wanted = left;
+#warning read holes
     filer->read_trunc(in->ino, &in->layout, in->snapid,
 		      pos, left, &tbl, 0,
 		      in->truncate_size, in->truncate_seq,
@@ -11432,7 +11438,8 @@ int Client::_read_sync(Fh *f, uint64_t off, uint64_t len, bufferlist *bl,
 
   if (r >= 0) {
     if (fscrypt_denc) {
-      r = fscrypt_denc->decrypt_bl(off, target_len, read_start, pbl);
+      std::vector<ObjectCacher::ObjHole> holes;
+      r = fscrypt_denc->decrypt_bl(off, target_len, read_start, holes, pbl);
       if (r < 0) {
         ldout(cct, 20) << __func__ << "(): failed to decrypt buffer: r=" << r << dendl;
       }
