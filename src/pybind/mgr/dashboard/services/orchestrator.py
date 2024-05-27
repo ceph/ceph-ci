e4cb6ff83f2db9b59438c07bee0fafad161a2169
Nizamudeen A <nia@redhat.com>
2024-05-27 10:05:21 +0530

    mgr/dashboard: use secure_channel for grpc requests
    Store the certificates to config-key stores and then later on used by
    dashboard to set-up the secure_channel for grpc nvmeof requests
    
    By storing the certificates we can ensure that the dashboard nvmeof apis
    will be configurable even if the deployments are not cephadm based
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 0a393ca9bde714524da369caaef1c097472f791c)
    (cherry picked from commit ebcb198894347d1a21ab4cd81fea985aca454b29)
    
    Resolves: rhbz#2282825
    
    Conflicts:
    	src/pybind/mgr/cephadm/services/nvmeof.py

--- src/pybind/mgr/dashboard/services/orchestrator.py
+++ src/pybind/mgr/dashboard/services/orchestrator.py
@@ -200,6 +200,26 @@ class UpgradeManager(ResourceManager):
         return self.api.upgrade_stop()
 
 
+class HardwareManager(ResourceManager):
+
+    @wait_api_result
+    def common(self, category: str, hostname: Optional[List[str]] = None) -> str:
+        return self.api.node_proxy_common(category, hostname=hostname)
+
+
+class CertStoreManager(ResourceManager):
+
+    @wait_api_result
+    def get_cert(self, entity: str, service_name: Optional[str] = None,
+                 hostname: Optional[str] = None) -> str:
+        return self.api.cert_store_get_cert(entity, service_name, hostname)
+
+    @wait_api_result
+    def get_key(self, entity: str, service_name: Optional[str] = None,
+                hostname: Optional[str] = None) -> str:
+        return self.api.cert_store_get_key(entity, service_name, hostname)
+
+
 class OrchClient(object):
 
     _instance = None
@@ -220,6 +240,8 @@ class OrchClient(object):
         self.osds = OsdManager(self.api)
         self.daemons = DaemonManager(self.api)
         self.upgrades = UpgradeManager(self.api)
+        self.hardware = HardwareManager(self.api)
+        self.cert_store = CertStoreManager(self.api)
 
     def available(self, features: Optional[List[str]] = None) -> bool:
         available = self.status()['available']
