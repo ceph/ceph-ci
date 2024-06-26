3912fd41d9756eb183ab4f630a9a22949f9c586b
Daniel Gryniewicz <dang@redhat.com>
2024-06-26 10:30:42 -0400

    rgw_cksum: introduce attribute caching in RadosMultipartUpload
    Allow MultipartUpload::complete() to have previously called
    Upload::get_info() without an additional round-trip to fetch
    attributes.
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 77f69974ef84ae3b65052c37e0db63517a8294bd)
    
    Fixes: https://tracker.ceph.com/issues/66837
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/driver/rados/rgw_sal_rados.cc
+++ src/rgw/driver/rados/rgw_sal_rados.cc
@@ -3545,6 +3545,7 @@ int RadosMultipartUpload::get_info(const DoutPrefixProvider *dpp, optional_yield
     return 0;
   }
 
+  /* Handle caching */
   if (rule) {
     if (!placement.empty()) {
       *rule = &placement;
@@ -3554,6 +3555,14 @@ int RadosMultipartUpload::get_info(const DoutPrefixProvider *dpp, optional_yield
       }
     } else {
       *rule = nullptr;
+    }
+  }
+
+  if (attrs) {
+    if (!cached_attrs.empty()) {
+      *attrs = cached_attrs;
+      if (!rule || *rule != nullptr)
+        return 0;
     }
   }
 
@@ -3577,11 +3586,13 @@ int RadosMultipartUpload::get_info(const DoutPrefixProvider *dpp, optional_yield
     return ret;
   }
 
+  /* Cache attrs filled in by prepare */
+  cached_attrs = meta_obj->get_attrs();
+
   extract_span_context(meta_obj->get_attrs(), trace_ctx);
 
   if (attrs) {
-    /* Attrs are filled in by prepare */
-    *attrs = meta_obj->get_attrs();
+    *attrs = cached_attrs;
     if (!rule || *rule != nullptr) {
       /* placement was cached; don't actually read */
       return 0;
