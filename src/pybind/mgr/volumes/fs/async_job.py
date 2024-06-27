ceph/ceph

mgr/vol: improvements and cleanup for "list_volumes()"
List volumes returns a list of of 1-member dictionaries where each
member is "{'name': <volname>}". This format is useful printing output
of the command "ceph fs volume ls" but for internal use this format is
hinderance since before using it, this list needs to be resolved  from
list of dictionaries to list of strings.

Return a list of strings and move the code for converting it to list of
dictionaries to method "list_fs_volumes()" (which is method run by "ceph
fs volume ls" command).

This triggers change in async_job.py where we need volnames. The code
here converts list of dicts to list of string. This needs to removed.

Second, don't create unnecessary temporary variable "fs_map" in
"list_volumes()" since it is not required. To get fs names we can
directly iterate.

Third, rename "list_volumes" method to a more precise names,
"get_all_volnames()".

Signed-off-by: Rishabh Dave <ridave@redhat.com>

src/pybind/mgr/volumes/fs/async_job.py
@@ -6,7 +6,7 @@ import traceback
 from collections import deque
 from mgr_util import lock_timeout_log, CephfsClient
 
-from .operations.volume import list_volumes
+from .operations.volume import get_all_volnames
 from .exception import NotImplementedException
 
 log = logging.getLogger(__name__)
@@ -44,8 +44,8 @@ class JobThread(threading.Thread):
                             return
                         timo = self.async_job.wakeup_timeout
                         if timo is not None:
-                            vols = [e['name'] for e in list_volumes(self.vc.mgr)]
-                            missing = set(vols) - set(self.async_job.q)
+                            volnames = get_all_volnames(self.vc.mgr)
+                            missing = set(volnames) - set(self.async_job.q)
                             for m in missing:
                                 self.async_job.jobs[m] = []
                                 self.async_job.q.append(m)

src/pybind/mgr/volumes/fs/volume.py
@@ -16,7 +16,7 @@ from .fs_util import listdir, has_subdir
 from .operations.group import open_group, create_group, remove_group, \
     open_group_unique, set_group_attrs
 from .operations.volume import create_volume, delete_volume, rename_volume, \
-    list_volumes, open_volume, get_pool_names, get_pool_ids, \
+    get_all_volnames, open_volume, get_pool_names, get_pool_ids, \
     get_pending_subvol_deletions_count, get_all_pending_clones_count
 from .operations.subvolume import open_subvol, create_subvol, remove_subvol, \
     create_clone
@@ -144,7 +144,9 @@ class VolumeClient(CephfsClient["Module"]):
         return delete_volume(self.mgr, volname, metadata_pool, data_pools)
 
     def list_fs_volumes(self):
-        volumes = list_volumes(self.mgr)
+        volnames = get_all_volnames(self.mgr)
+        # since we report in json format, make a dict of volnames.
+        volumes = [{'name': vn} for vn in volnames]
         return 0, json.dumps(volumes, indent=4, sort_keys=True), ""
 
     def rename_fs_volume(self, volname, newvolname, sure):

ceph/ceph-ci

src/pybind/mgr/volumes/fs/async_job.py
@@ -44,8 +44,8 @@ class JobThread(threading.Thread):
                             return
                         timo = self.async_job.wakeup_timeout
                         if timo is not None:
-                            vols = [e['name'] for e in list_volumes(self.vc.mgr)]
-                            missing = set(vols) - set(self.async_job.q)
+                            volnames = list_volumes(self.vc.mgr)
+                            missing = set(volnames) - set(self.async_job.q)
                             for m in missing:
                                 self.async_job.jobs[m] = []
                                 self.async_job.q.append(m)

src/pybind/mgr/volumes/fs/volume.py
@@ -144,7 +144,9 @@ class VolumeClient(CephfsClient["Module"]):
         return delete_volume(self.mgr, volname, metadata_pool, data_pools)
 
     def list_fs_volumes(self):
-        volumes = list_volumes(self.mgr)
+        volnames = list_volumes(self.mgr)
+        # since we report in json format, make a dict of volnames.
+        volumes = [{'name': vn} for vn in volnames]
         return 0, json.dumps(volumes, indent=4, sort_keys=True), ""
 
     def rename_fs_volume(self, volname, newvolname, sure):