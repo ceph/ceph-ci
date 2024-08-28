692774f047896a1bfcdfadae63c252b767c582cd
Adam King <adking@redhat.com>
2024-08-28 13:46:59 -0400

    mgr/orchestrator: allow passing group to apply/add nvmeof commands
    We no longer require the group when applying an nvmeof spec
    but we still want to allow the commands to take a group
    parameter (and this will at least make a group name
    required when creating a new service on the command line)
    
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit b377085c3020484c74a7a71357c35761b5e5de25)
    
    Resolves: rhbz#2304295

--- src/pybind/mgr/orchestrator/module.py
+++ src/pybind/mgr/orchestrator/module.py
@@ -1526,6 +1526,7 @@ Usage:
     @_cli_write_command('orch daemon add nvmeof')
     def _nvmeof_add(self,
                     pool: str,
+                    group: str,
                     placement: Optional[str] = None,
                     inbuf: Optional[str] = None) -> HandleCommandResult:
         """Start nvmeof daemon(s)"""
@@ -1535,6 +1536,7 @@ Usage:
         spec = NvmeofServiceSpec(
             service_id='nvmeof',
             pool=pool,
+            group=group,
             placement=PlacementSpec.from_string(placement),
         )
         return self._daemon_add_misc(spec)
@@ -1824,6 +1826,7 @@ Usage:
     @_cli_write_command('orch apply nvmeof')
     def _apply_nvmeof(self,
                       pool: str,
+                      group: str,
                       placement: Optional[str] = None,
                       unmanaged: bool = False,
                       dry_run: bool = False,
@@ -1835,8 +1838,9 @@ Usage:
             raise OrchestratorValidationError('unrecognized command -i; -h or --help for usage')
 
         spec = NvmeofServiceSpec(
-            service_id=pool,
+            service_id=f'{pool}.{group}',
             pool=pool,
+            group=group,
             placement=PlacementSpec.from_string(placement),
             unmanaged=unmanaged,
             preview_only=dry_run
