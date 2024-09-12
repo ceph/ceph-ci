336fc7f5b8d295a62d5f53f5fa7efa88ae4914c3
Justin Caratzas <jcaratza@ibm.com>
2024-09-12 13:12:20 -0400

    Revert "osd: EC Partial Stripe Reads (Retry of #23138 and #52746)"
    This reverts commit fb32fbfeaacf062862ae97045f9719737cab3ecf.
    
    Signed-off-by: Justin Caratzas <jcaratza@ibm.com>

--- src/test/erasure-code/TestErasureCodeExample.cc
+++ src/test/erasure-code/TestErasureCodeExample.cc
@@ -197,14 +197,9 @@ TEST(ErasureCodeExample, decode)
   usable.substr_of(out, 0, in.length());
   EXPECT_TRUE(usable == in);
 
-  // partial chunk decode
-  map<int, bufferlist> partial_decode;
-  partial_decode[0] = encoded[0];
-  EXPECT_EQ(0, example.decode_concat(partial_decode, &out));
-
   // cannot recover
   map<int, bufferlist> degraded;  
-  degraded[2] = encoded[2];
+  degraded[0] = encoded[0];
   EXPECT_EQ(-ERANGE, example.decode_concat(degraded, &out));
 }
 
