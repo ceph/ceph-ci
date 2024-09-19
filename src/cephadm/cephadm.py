d0f8135e8e1b97360c3aa1ed34cb484699ad55ce
Adam King <adking@redhat.com>
2024-09-19 07:33:47 -0400

    cephadm: support --yes-i-know flag for bootstrap
    Purely for backwards compatability reasons. This flag
    has no actual effect when passed during bootstrap.
    
    Signed-off-by: Adam King <adking@redhat.com>

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -5607,6 +5607,11 @@ def _get_parser():
     parser_bootstrap.add_argument(
         '--custom-prometheus-alerts',
         help='provide a file with custom prometheus alerts')
+    parser_bootstrap.add_argument(
+        '--yes-i-know',
+        action='store_true',
+        help='deprecated flag only here for backwards compatibility')
+
 
     parser_bootstrap.add_argument(
         '--call-home-icn',
