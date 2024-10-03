ceph/ceph

mgr/dashboard: RGW multisite sync remove zones fix
Fixes: https://tracker.ceph.com/issues/66945

Signed-off-by: Naman Munet <nmunet@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/models/rgw-multisite.ts
@@ -61,3 +61,8 @@ export enum FlowType {
   directional = 'directional',
   symmetrical = 'symmetrical'
 }
+
+export interface ZoneType {
+  added: string[];
+  removed: string[];
+}

ceph/ceph-ci

Resolves: rhbz#2303394
(cherry picked from commit 5eee7e8105167dc4f0542f62d041fbcb143611d0)
(cherry picked from commit c1dadedb6d133a29fa557afdac4b8f47ff8b7a9f)
 Conflicts:
	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-sync-policy-details/rgw-multisite-sync-policy-details.component.ts
	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw.module.ts

src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/models/rgw-multisite.ts
@@ -61,3 +61,8 @@ export enum FlowType {
   directional = 'directional',
   symmetrical = 'symmetrical'
 }
+
+export interface Zone {
+  added: string[];
+  removed: string[];
+}