ceph/ceph

Initial version of the libcephfs proxy
The libcephfs proxy is composed by a new daemon process (libcephfsd) and
a new library (libcephfs_proxy.so) that allow client applications to
work without directly connecting to the libcephfs.so library.

The libcephfs_proxy.so library exports binary compatible versions of the
libcephfs.so functions. These functions forward the application requests
to the libcephfsd daemon running on the same machine through a UNIX
socket. The daemon, which is linked to the real libcephfs.so library,
executes the incoming requests and sends back the answers.

The client application doesn't need to be modified to use the proxy. It
just needs to link against libcephfs_proxy.so instead of libcephfs.so,
or even use LD_PRELOAD when executing it.

The main advantage of using the proxy is that all ceph mounts are
managed in a single place (the libcephfsd daemon), which will help
reduce memory usage when many processes are connecting to the same Ceph
volumes using the same configuration.

Signed-off-by: Xavi Hernandez <xhernandez@gmail.com>

src/proxy/proxy.h
@@ -1,0 +1,64 @@
+
+#ifndef __LIBCEPHFSD_PROXY_H__
+#define __LIBCEPHFSD_PROXY_H__
+
+#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define LIBCEPHFSD_MAJOR 0
+#define LIBCEPHFSD_MINOR 2
+
+#define LIBCEPHFS_LIB_CLIENT 0xe3e5f0e8 // 'ceph' xor 0x80808080
+
+#define offset_of(_type, _field) ((uintptr_t) & ((_type *)0)->_field)
+
+#define container_of(_ptr, _type, _field) \
+	((_type *)((uintptr_t)(_ptr) - offset_of(_type, _field)))
+
+struct _list;
+typedef struct _list list_t;
+
+struct _proxy_buffer_ops;
+typedef struct _proxy_buffer_ops proxy_buffer_ops_t;
+
+struct _proxy_buffer;
+typedef struct _proxy_buffer proxy_buffer_t;
+
+struct _proxy_output;
+typedef struct _proxy_output proxy_output_t;
+
+struct _proxy_log_handler;
+typedef struct _proxy_log_handler proxy_log_handler_t;
+
+struct _proxy_worker;
+typedef struct _proxy_worker proxy_worker_t;
+
+struct _proxy_manager;
+typedef struct _proxy_manager proxy_manager_t;
+
+struct _proxy_link;
+typedef struct _proxy_link proxy_link_t;
+
+typedef int32_t (*proxy_output_write_t)(proxy_output_t *);
+typedef int32_t (*proxy_output_full_t)(proxy_output_t *);
+
+typedef void (*proxy_log_callback_t)(proxy_log_handler_t *, int32_t, int32_t,
+				     const char *);
+
+typedef void (*proxy_worker_main_t)(proxy_worker_t *);
+typedef void (*proxy_worker_destroy_t)(proxy_worker_t *);
+
+typedef int32_t (*proxy_manager_main_t)(proxy_manager_t *);
+
+typedef int32_t (*proxy_link_main_t)(proxy_link_t *, int32_t);
+typedef bool (*proxy_link_stop_t)(proxy_link_t *);
+
+struct _list {
+	list_t *next;
+	list_t *prev;
+};
+
+#endif

src/proxy/proxy_helpers.h
@@ -1,0 +1,311 @@
+
+#ifndef __LIBCEPHFS_PROXY_HELPERS_H__
+#define __LIBCEPHFS_PROXY_HELPERS_H__
+
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "proxy_log.h"
+
+#define __public __attribute__((__visibility__("default")))
+
+#define ptr_value(_ptr) ((uint64_t)(uintptr_t)(_ptr))
+#define value_ptr(_val) ((void *)(uintptr_t)(_val))
+
+typedef struct _proxy_random {
+	uint64_t mask;
+	uint64_t factor;
+	uint64_t factor_inv;
+	uint64_t shift;
+} proxy_random_t;
+
+/* Generate a 64-bits random number different than 0. */
+static inline uint64_t random_u64(void)
+{
+	uint64_t value;
+	int32_t i;
+
+	do {
+		value = 0;
+		for (i = 0; i < 4; i++) {
+			value <<= 16;
+			value ^= (random() >> 8) & 0xffff;
+		}
+	} while (value == 0);
+
+	return value;
+}
+
+/* Randomly initialize the data used to scramble pointers. */
+static inline void random_init(proxy_random_t *rnd)
+{
+	uint64_t inv;
+
+	rnd->mask = random_u64();
+
+	/* Generate an odd multiplicative factor different than 1. */
+	do {
+		rnd->factor = random_u64() | 1;
+	} while (rnd->factor == 1);
+
+	/* Compute the inverse of 'factor' modulo 2^64. */
+	inv = rnd->factor & 0x3;
+	inv *= 0x000000012 - rnd->factor * inv;
+	inv *= 0x000000102 - rnd->factor * inv;
+	inv *= 0x000010002 - rnd->factor * inv;
+	inv *= 0x100000002 - rnd->factor * inv;
+	rnd->factor_inv = inv * (2 - rnd->factor * inv);
+
+	rnd->shift = random_u64();
+}
+
+/* Obfuscate a pointer. */
+static inline uint64_t random_scramble(proxy_random_t *rnd, uint64_t value)
+{
+	uint32_t bits;
+
+	bits = __builtin_popcountll(value);
+
+	/* rnd->shift is rotated by the amount of bits set to 1 in the original
+	 * value, and the lowest 6 bits are extracted. This generates a
+	 * pseudo-random number that depends on the number of bits of the
+	 * value. */
+	bits = ((rnd->shift >> bits) | (rnd->shift << (64 - bits))) & 0x3f;
+
+	/* The value is rotated by the amount just computed. */
+	value = (value << bits) | (value >> (64 - bits));
+
+	/* The final result is masked with a random number. */
+	value ^= rnd->mask;
+
+	/* And multiplied by a random factor modulo 2^64. */
+	return value * rnd->factor;
+}
+
+/* Recover a pointer. */
+static inline uint64_t random_unscramble(proxy_random_t *rnd, uint64_t value)
+{
+	uint32_t bits;
+
+	/* Divide by the random factor (i.e. multiply by the inverse of the
+	 * factor). */
+	value *= rnd->factor_inv;
+
+	/* Remove the mask. */
+	value ^= rnd->mask;
+
+	/* Get the number of bits the pointer was rotated. */
+	bits = __builtin_popcountll(value);
+	bits = ((rnd->shift >> bits) | (rnd->shift << (64 - bits))) & 0x3f;
+
+	/* Undo the rotation to recover the original value. */
+	return (value >> bits) | (value << (64 - bits));
+}
+
+static inline void *proxy_malloc(size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(size);
+	if (ptr == NULL) {
+		proxy_log(LOG_ERR, errno, "Failed to allocate memory");
+	}
+
+	return ptr;
+}
+
+static inline int32_t proxy_realloc(void **pptr, size_t size)
+{
+	void *ptr;
+
+	ptr = realloc(*pptr, size);
+	if (ptr == NULL) {
+		return proxy_log(LOG_ERR, errno, "Failed to reallocate memory");
+	}
+
+	*pptr = ptr;
+
+	return 0;
+}
+
+static inline void proxy_free(void *ptr)
+{
+	free(ptr);
+}
+
+static inline char *proxy_strdup(const char *str)
+{
+	char *ptr;
+
+	ptr = strdup(str);
+	if (ptr == NULL) {
+		proxy_log(LOG_ERR, errno, "Failed to copy a string");
+		return NULL;
+	}
+
+	return ptr;
+}
+
+static inline int32_t proxy_mutex_init(pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_mutex_init(mutex, NULL);
+	if (err != 0) {
+		return proxy_log(LOG_ERR, err, "Failed to initialize a mutex");
+	}
+
+	return 0;
+}
+
+static inline void proxy_mutex_lock(pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_mutex_lock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "Mutex cannot be acquired");
+	}
+}
+
+static inline void proxy_mutex_unlock(pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_mutex_unlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "Mutex cannot be released");
+	}
+}
+
+static inline int32_t proxy_rwmutex_init(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_init(mutex, NULL);
+	if (err != 0) {
+		return proxy_log(LOG_ERR, err,
+				 "Failed to initialize a rwmutex");
+	}
+
+	return 0;
+}
+
+static inline void proxy_rwmutex_rdlock(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_rdlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "RWMutex cannot be acquired for read");
+	}
+}
+
+static inline void proxy_rwmutex_wrlock(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_wrlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "RWMutex cannot be acquired for write");
+	}
+}
+
+static inline void proxy_rwmutex_unlock(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_unlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "RWMutex cannot be released");
+	}
+}
+
+static inline int32_t proxy_condition_init(pthread_cond_t *condition)
+{
+	int32_t err;
+
+	err = pthread_cond_init(condition, NULL);
+	if (err != 0) {
+		return proxy_log(LOG_ERR, err,
+				 "Failed to initialize a condition variable");
+	}
+
+	return 0;
+}
+
+static inline void proxy_condition_signal(pthread_cond_t *condition)
+{
+	int32_t err;
+
+	err = pthread_cond_signal(condition);
+	if (err != 0) {
+		proxy_abort(err, "Condition variable cannot be signaled");
+	}
+}
+
+static inline void proxy_condition_wait(pthread_cond_t *condition,
+					pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_cond_wait(condition, mutex);
+	if (err != 0) {
+		proxy_abort(err, "Condition variable cannot be waited");
+	}
+}
+
+static inline int32_t proxy_thread_create(pthread_t *tid, void *(*main)(void *),
+					  void *arg)
+{
+	int32_t err;
+
+	err = pthread_create(tid, NULL, main, arg);
+	if (err != 0) {
+		proxy_log(LOG_ERR, err, "Failed to create a thread");
+	}
+
+	return err;
+}
+
+static inline void proxy_thread_kill(pthread_t tid, int32_t signum)
+{
+	int32_t err;
+
+	err = pthread_kill(tid, signum);
+	if (err != 0) {
+		proxy_abort(err, "Failed to send a signal to a thread");
+	}
+}
+
+static inline void proxy_thread_join(pthread_t tid)
+{
+	int32_t err;
+
+	err = pthread_join(tid, NULL);
+	if (err != 0) {
+		proxy_log(LOG_ERR, err, "Unable to join a thread");
+	}
+}
+
+static inline int32_t proxy_signal_set(int32_t signum, struct sigaction *action,
+				       struct sigaction *old)
+{
+	if (sigaction(signum, action, old) < 0) {
+		return proxy_log(LOG_ERR, errno,
+				 "Failed to configure a signal");
+	}
+
+	return 0;
+}
+
+int32_t proxy_hash(uint8_t *hash, size_t size,
+		   int32_t (*feed)(void **, void *, int32_t), void *data);
+
+int32_t proxy_hash_hex(char *digest, size_t size,
+		       int32_t (*feed)(void **, void *, int32_t), void *data);
+
+#endif

src/proxy/proxy_manager.c
@@ -1,0 +1,247 @@
+
+#include <signal.h>
+
+#include "proxy_manager.h"
+#include "proxy_helpers.h"
+#include "proxy_list.h"
+#include "proxy_log.h"
+
+static void proxy_manager_signal_handler(int32_t signum, siginfo_t *info,
+					 void *ctx)
+{
+}
+
+static void proxy_worker_register(proxy_worker_t *worker)
+{
+	proxy_manager_t *manager;
+
+	manager = worker->manager;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	list_add_tail(&worker->list, &manager->workers);
+
+	proxy_mutex_unlock(&manager->mutex);
+}
+
+static void proxy_worker_deregister(proxy_worker_t *worker)
+{
+	proxy_manager_t *manager;
+
+	manager = worker->manager;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	list_del_init(&worker->list);
+	if (list_empty(&manager->workers)) {
+		proxy_condition_signal(&manager->condition);
+	}
+
+	proxy_mutex_unlock(&manager->mutex);
+}
+
+static void proxy_worker_finished(proxy_worker_t *worker)
+{
+	proxy_manager_t *manager;
+
+	manager = worker->manager;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	if (list_empty(&manager->finished)) {
+		proxy_condition_signal(&manager->condition);
+	}
+
+	list_move_tail(&worker->list, &manager->finished);
+
+	proxy_mutex_unlock(&manager->mutex);
+}
+
+static void *proxy_worker_main(void *arg)
+{
+	proxy_worker_t *worker;
+
+	worker = arg;
+
+	worker->main(worker);
+
+	proxy_worker_finished(worker);
+
+	return NULL;
+}
+
+static void *proxy_manager_main(void *arg)
+{
+	proxy_manager_t *manager;
+	proxy_worker_t *worker;
+
+	manager = arg;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	while (true) {
+		while (!list_empty(&manager->finished)) {
+			worker = list_first_entry(&manager->finished,
+						  proxy_worker_t, list);
+			list_del_init(&worker->list);
+
+			proxy_mutex_unlock(&manager->mutex);
+
+			proxy_thread_join(worker->tid);
+
+			if (worker->destroy != NULL) {
+				worker->destroy(worker);
+			}
+
+			proxy_mutex_lock(&manager->mutex);
+		}
+
+		if (manager->stop && list_empty(&manager->workers)) {
+			break;
+		}
+
+		proxy_condition_wait(&manager->condition, &manager->mutex);
+	}
+
+	manager->done = true;
+	proxy_condition_signal(&manager->condition);
+
+	proxy_mutex_unlock(&manager->mutex);
+
+	return NULL;
+}
+
+static int32_t proxy_manager_init(proxy_manager_t *manager)
+{
+	int32_t err;
+
+	list_init(&manager->workers);
+	list_init(&manager->finished);
+
+	manager->stop = false;
+	manager->done = false;
+
+	manager->main_tid = pthread_self();
+
+	err = proxy_mutex_init(&manager->mutex);
+	if (err < 0) {
+		return err;
+	}
+
+	err = proxy_condition_init(&manager->condition);
+	if (err < 0) {
+		pthread_mutex_destroy(&manager->mutex);
+	}
+
+	return err;
+}
+
+static void proxy_manager_destroy(proxy_manager_t *manager)
+{
+	pthread_cond_destroy(&manager->condition);
+	pthread_mutex_destroy(&manager->mutex);
+}
+
+static int32_t proxy_manager_setup_signals(struct sigaction *old)
+{
+	struct sigaction action;
+
+	/* The CONT signal will be used to wake threads blocked in I/O. */
+	memset(&action, 0, sizeof(action));
+	action.sa_flags = SA_SIGINFO;
+	action.sa_sigaction = proxy_manager_signal_handler;
+
+	return proxy_signal_set(SIGCONT, &action, old);
+}
+
+static void proxy_manager_restore_signals(struct sigaction *action)
+{
+	proxy_signal_set(SIGCONT, action, NULL);
+}
+
+static void proxy_manager_terminate(proxy_manager_t *manager)
+{
+	proxy_worker_t *worker;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	list_for_each_entry(worker, &manager->workers, list) {
+		worker->stop = true;
+		proxy_thread_kill(worker->tid, SIGCONT);
+	}
+
+	while (!manager->done) {
+		proxy_condition_wait(&manager->condition, &manager->mutex);
+	}
+
+	proxy_mutex_unlock(&manager->mutex);
+
+	proxy_thread_join(manager->tid);
+}
+
+int32_t proxy_manager_run(proxy_manager_t *manager, proxy_manager_main_t main)
+{
+	struct sigaction old_action;
+	int32_t err;
+
+	err = proxy_manager_init(manager);
+	if (err < 0) {
+		return err;
+	}
+
+	err = proxy_manager_setup_signals(&old_action);
+	if (err < 0) {
+		goto done_destroy;
+	}
+
+	err = proxy_thread_create(&manager->tid, proxy_manager_main, manager);
+	if (err < 0) {
+		goto done_signal;
+	}
+
+	err = main(manager);
+
+	proxy_manager_terminate(manager);
+
+done_signal:
+	proxy_manager_restore_signals(&old_action);
+
+done_destroy:
+	proxy_manager_destroy(manager);
+
+	return err;
+}
+
+void proxy_manager_shutdown(proxy_manager_t *manager)
+{
+	proxy_mutex_lock(&manager->mutex);
+
+	manager->stop = true;
+	proxy_condition_signal(&manager->condition);
+
+	proxy_mutex_unlock(&manager->mutex);
+
+	/* Wake the thread if it was blocked in an I/O operation. */
+	proxy_thread_kill(manager->main_tid, SIGCONT);
+}
+
+int32_t proxy_manager_launch(proxy_manager_t *manager, proxy_worker_t *worker,
+			     proxy_worker_main_t main,
+			     proxy_worker_destroy_t destroy)
+{
+	int32_t err;
+
+	worker->manager = manager;
+	worker->main = main;
+	worker->destroy = destroy;
+	worker->stop = false;
+
+	proxy_worker_register(worker);
+
+	err = proxy_thread_create(&worker->tid, proxy_worker_main, worker);
+	if (err < 0) {
+		proxy_worker_deregister(worker);
+	}
+
+	return err;
+}

ceph/ceph-ci

src/libcephfs_proxy/proxy.h
@@ -1,0 +1,67 @@
+
+#ifndef __LIBCEPHFSD_PROXY_H__
+#define __LIBCEPHFSD_PROXY_H__
+
+#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define LIBCEPHFSD_MAJOR 0
+#define LIBCEPHFSD_MINOR 2
+
+#define LIBCEPHFS_LIB_CLIENT 0xe3e5f0e8 // 'ceph' xor 0x80808080
+
+#define PROXY_SOCKET "/run/libcephfsd.sock"
+#define PROXY_SOCKET_ENV "LIBCEPHFSD_SOCKET"
+
+#define offset_of(_type, _field) ((uintptr_t) & ((_type *)0)->_field)
+
+#define container_of(_ptr, _type, _field) \
+	((_type *)((uintptr_t)(_ptr) - offset_of(_type, _field)))
+
+struct _list;
+typedef struct _list list_t;
+
+struct _proxy_buffer_ops;
+typedef struct _proxy_buffer_ops proxy_buffer_ops_t;
+
+struct _proxy_buffer;
+typedef struct _proxy_buffer proxy_buffer_t;
+
+struct _proxy_output;
+typedef struct _proxy_output proxy_output_t;
+
+struct _proxy_log_handler;
+typedef struct _proxy_log_handler proxy_log_handler_t;
+
+struct _proxy_worker;
+typedef struct _proxy_worker proxy_worker_t;
+
+struct _proxy_manager;
+typedef struct _proxy_manager proxy_manager_t;
+
+struct _proxy_link;
+typedef struct _proxy_link proxy_link_t;
+
+typedef int32_t (*proxy_output_write_t)(proxy_output_t *);
+typedef int32_t (*proxy_output_full_t)(proxy_output_t *);
+
+typedef void (*proxy_log_callback_t)(proxy_log_handler_t *, int32_t, int32_t,
+				     const char *);
+
+typedef void (*proxy_worker_start_t)(proxy_worker_t *);
+typedef void (*proxy_worker_destroy_t)(proxy_worker_t *);
+
+typedef int32_t (*proxy_manager_start_t)(proxy_manager_t *);
+
+typedef int32_t (*proxy_link_start_t)(proxy_link_t *, int32_t);
+typedef bool (*proxy_link_stop_t)(proxy_link_t *);
+
+struct _list {
+	list_t *next;
+	list_t *prev;
+};
+
+#endif

src/libcephfs_proxy/proxy_helpers.h
@@ -1,0 +1,311 @@
+
+#ifndef __LIBCEPHFS_PROXY_HELPERS_H__
+#define __LIBCEPHFS_PROXY_HELPERS_H__
+
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "proxy_log.h"
+
+#define __public __attribute__((__visibility__("default")))
+
+#define ptr_value(_ptr) ((uint64_t)(uintptr_t)(_ptr))
+#define value_ptr(_val) ((void *)(uintptr_t)(_val))
+
+typedef struct _proxy_random {
+	uint64_t mask;
+	uint64_t factor;
+	uint64_t factor_inv;
+	uint64_t shift;
+} proxy_random_t;
+
+/* Generate a 64-bits random number different than 0. */
+static inline uint64_t random_u64(void)
+{
+	uint64_t value;
+	int32_t i;
+
+	do {
+		value = 0;
+		for (i = 0; i < 4; i++) {
+			value <<= 16;
+			value ^= (random() >> 8) & 0xffff;
+		}
+	} while (value == 0);
+
+	return value;
+}
+
+/* Randomly initialize the data used to scramble pointers. */
+static inline void random_init(proxy_random_t *rnd)
+{
+	uint64_t inv;
+
+	rnd->mask = random_u64();
+
+	/* Generate an odd multiplicative factor different than 1. */
+	do {
+		rnd->factor = random_u64() | 1;
+	} while (rnd->factor == 1);
+
+	/* Compute the inverse of 'factor' modulo 2^64. */
+	inv = rnd->factor & 0x3;
+	inv *= 0x000000012 - rnd->factor * inv;
+	inv *= 0x000000102 - rnd->factor * inv;
+	inv *= 0x000010002 - rnd->factor * inv;
+	inv *= 0x100000002 - rnd->factor * inv;
+	rnd->factor_inv = inv * (2 - rnd->factor * inv);
+
+	rnd->shift = random_u64();
+}
+
+/* Obfuscate a pointer. */
+static inline uint64_t random_scramble(proxy_random_t *rnd, uint64_t value)
+{
+	uint32_t bits;
+
+	bits = __builtin_popcountll(value);
+
+	/* rnd->shift is rotated by the amount of bits set to 1 in the original
+	 * value, and the lowest 6 bits are extracted. This generates a
+	 * pseudo-random number that depends on the number of bits of the
+	 * value. */
+	bits = ((rnd->shift >> bits) | (rnd->shift << (64 - bits))) & 0x3f;
+
+	/* The value is rotated by the amount just computed. */
+	value = (value << bits) | (value >> (64 - bits));
+
+	/* The final result is masked with a random number. */
+	value ^= rnd->mask;
+
+	/* And multiplied by a random factor modulo 2^64. */
+	return value * rnd->factor;
+}
+
+/* Recover a pointer. */
+static inline uint64_t random_unscramble(proxy_random_t *rnd, uint64_t value)
+{
+	uint32_t bits;
+
+	/* Divide by the random factor (i.e. multiply by the inverse of the
+	 * factor). */
+	value *= rnd->factor_inv;
+
+	/* Remove the mask. */
+	value ^= rnd->mask;
+
+	/* Get the number of bits the pointer was rotated. */
+	bits = __builtin_popcountll(value);
+	bits = ((rnd->shift >> bits) | (rnd->shift << (64 - bits))) & 0x3f;
+
+	/* Undo the rotation to recover the original value. */
+	return (value >> bits) | (value << (64 - bits));
+}
+
+static inline void *proxy_malloc(size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(size);
+	if (ptr == NULL) {
+		proxy_log(LOG_ERR, errno, "Failed to allocate memory");
+	}
+
+	return ptr;
+}
+
+static inline int32_t proxy_realloc(void **pptr, size_t size)
+{
+	void *ptr;
+
+	ptr = realloc(*pptr, size);
+	if (ptr == NULL) {
+		return proxy_log(LOG_ERR, errno, "Failed to reallocate memory");
+	}
+
+	*pptr = ptr;
+
+	return 0;
+}
+
+static inline void proxy_free(void *ptr)
+{
+	free(ptr);
+}
+
+static inline char *proxy_strdup(const char *str)
+{
+	char *ptr;
+
+	ptr = strdup(str);
+	if (ptr == NULL) {
+		proxy_log(LOG_ERR, errno, "Failed to copy a string");
+		return NULL;
+	}
+
+	return ptr;
+}
+
+static inline int32_t proxy_mutex_init(pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_mutex_init(mutex, NULL);
+	if (err != 0) {
+		return proxy_log(LOG_ERR, err, "Failed to initialize a mutex");
+	}
+
+	return 0;
+}
+
+static inline void proxy_mutex_lock(pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_mutex_lock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "Mutex cannot be acquired");
+	}
+}
+
+static inline void proxy_mutex_unlock(pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_mutex_unlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "Mutex cannot be released");
+	}
+}
+
+static inline int32_t proxy_rwmutex_init(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_init(mutex, NULL);
+	if (err != 0) {
+		return proxy_log(LOG_ERR, err,
+				 "Failed to initialize a rwmutex");
+	}
+
+	return 0;
+}
+
+static inline void proxy_rwmutex_rdlock(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_rdlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "RWMutex cannot be acquired for read");
+	}
+}
+
+static inline void proxy_rwmutex_wrlock(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_wrlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "RWMutex cannot be acquired for write");
+	}
+}
+
+static inline void proxy_rwmutex_unlock(pthread_rwlock_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_rwlock_unlock(mutex);
+	if (err != 0) {
+		proxy_abort(err, "RWMutex cannot be released");
+	}
+}
+
+static inline int32_t proxy_condition_init(pthread_cond_t *condition)
+{
+	int32_t err;
+
+	err = pthread_cond_init(condition, NULL);
+	if (err != 0) {
+		return proxy_log(LOG_ERR, err,
+				 "Failed to initialize a condition variable");
+	}
+
+	return 0;
+}
+
+static inline void proxy_condition_signal(pthread_cond_t *condition)
+{
+	int32_t err;
+
+	err = pthread_cond_signal(condition);
+	if (err != 0) {
+		proxy_abort(err, "Condition variable cannot be signaled");
+	}
+}
+
+static inline void proxy_condition_wait(pthread_cond_t *condition,
+					pthread_mutex_t *mutex)
+{
+	int32_t err;
+
+	err = pthread_cond_wait(condition, mutex);
+	if (err != 0) {
+		proxy_abort(err, "Condition variable cannot be waited");
+	}
+}
+
+static inline int32_t proxy_thread_create(pthread_t *tid,
+					  void *(*start)(void *), void *arg)
+{
+	int32_t err;
+
+	err = pthread_create(tid, NULL, start, arg);
+	if (err != 0) {
+		proxy_log(LOG_ERR, err, "Failed to create a thread");
+	}
+
+	return err;
+}
+
+static inline void proxy_thread_kill(pthread_t tid, int32_t signum)
+{
+	int32_t err;
+
+	err = pthread_kill(tid, signum);
+	if (err != 0) {
+		proxy_abort(err, "Failed to send a signal to a thread");
+	}
+}
+
+static inline void proxy_thread_join(pthread_t tid)
+{
+	int32_t err;
+
+	err = pthread_join(tid, NULL);
+	if (err != 0) {
+		proxy_log(LOG_ERR, err, "Unable to join a thread");
+	}
+}
+
+static inline int32_t proxy_signal_set(int32_t signum, struct sigaction *action,
+				       struct sigaction *old)
+{
+	if (sigaction(signum, action, old) < 0) {
+		return proxy_log(LOG_ERR, errno,
+				 "Failed to configure a signal");
+	}
+
+	return 0;
+}
+
+int32_t proxy_hash(uint8_t *hash, size_t size,
+		   int32_t (*feed)(void **, void *, int32_t), void *data);
+
+int32_t proxy_hash_hex(char *digest, size_t size,
+		       int32_t (*feed)(void **, void *, int32_t), void *data);
+
+#endif

src/libcephfs_proxy/proxy_manager.c
@@ -1,0 +1,247 @@
+
+#include <signal.h>
+
+#include "proxy_manager.h"
+#include "proxy_helpers.h"
+#include "proxy_list.h"
+#include "proxy_log.h"
+
+static void proxy_manager_signal_handler(int32_t signum, siginfo_t *info,
+					 void *ctx)
+{
+}
+
+static void proxy_worker_register(proxy_worker_t *worker)
+{
+	proxy_manager_t *manager;
+
+	manager = worker->manager;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	list_add_tail(&worker->list, &manager->workers);
+
+	proxy_mutex_unlock(&manager->mutex);
+}
+
+static void proxy_worker_deregister(proxy_worker_t *worker)
+{
+	proxy_manager_t *manager;
+
+	manager = worker->manager;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	list_del_init(&worker->list);
+	if (list_empty(&manager->workers)) {
+		proxy_condition_signal(&manager->condition);
+	}
+
+	proxy_mutex_unlock(&manager->mutex);
+}
+
+static void proxy_worker_finished(proxy_worker_t *worker)
+{
+	proxy_manager_t *manager;
+
+	manager = worker->manager;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	if (list_empty(&manager->finished)) {
+		proxy_condition_signal(&manager->condition);
+	}
+
+	list_move_tail(&worker->list, &manager->finished);
+
+	proxy_mutex_unlock(&manager->mutex);
+}
+
+static void *proxy_worker_start(void *arg)
+{
+	proxy_worker_t *worker;
+
+	worker = arg;
+
+	worker->start(worker);
+
+	proxy_worker_finished(worker);
+
+	return NULL;
+}
+
+static void *proxy_manager_start(void *arg)
+{
+	proxy_manager_t *manager;
+	proxy_worker_t *worker;
+
+	manager = arg;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	while (true) {
+		while (!list_empty(&manager->finished)) {
+			worker = list_first_entry(&manager->finished,
+						  proxy_worker_t, list);
+			list_del_init(&worker->list);
+
+			proxy_mutex_unlock(&manager->mutex);
+
+			proxy_thread_join(worker->tid);
+
+			if (worker->destroy != NULL) {
+				worker->destroy(worker);
+			}
+
+			proxy_mutex_lock(&manager->mutex);
+		}
+
+		if (manager->stop && list_empty(&manager->workers)) {
+			break;
+		}
+
+		proxy_condition_wait(&manager->condition, &manager->mutex);
+	}
+
+	manager->done = true;
+	proxy_condition_signal(&manager->condition);
+
+	proxy_mutex_unlock(&manager->mutex);
+
+	return NULL;
+}
+
+static int32_t proxy_manager_init(proxy_manager_t *manager)
+{
+	int32_t err;
+
+	list_init(&manager->workers);
+	list_init(&manager->finished);
+
+	manager->stop = false;
+	manager->done = false;
+
+	manager->main_tid = pthread_self();
+
+	err = proxy_mutex_init(&manager->mutex);
+	if (err < 0) {
+		return err;
+	}
+
+	err = proxy_condition_init(&manager->condition);
+	if (err < 0) {
+		pthread_mutex_destroy(&manager->mutex);
+	}
+
+	return err;
+}
+
+static void proxy_manager_destroy(proxy_manager_t *manager)
+{
+	pthread_cond_destroy(&manager->condition);
+	pthread_mutex_destroy(&manager->mutex);
+}
+
+static int32_t proxy_manager_setup_signals(struct sigaction *old)
+{
+	struct sigaction action;
+
+	/* The CONT signal will be used to wake threads blocked in I/O. */
+	memset(&action, 0, sizeof(action));
+	action.sa_flags = SA_SIGINFO;
+	action.sa_sigaction = proxy_manager_signal_handler;
+
+	return proxy_signal_set(SIGCONT, &action, old);
+}
+
+static void proxy_manager_restore_signals(struct sigaction *action)
+{
+	proxy_signal_set(SIGCONT, action, NULL);
+}
+
+static void proxy_manager_terminate(proxy_manager_t *manager)
+{
+	proxy_worker_t *worker;
+
+	proxy_mutex_lock(&manager->mutex);
+
+	list_for_each_entry(worker, &manager->workers, list) {
+		worker->stop = true;
+		proxy_thread_kill(worker->tid, SIGCONT);
+	}
+
+	while (!manager->done) {
+		proxy_condition_wait(&manager->condition, &manager->mutex);
+	}
+
+	proxy_mutex_unlock(&manager->mutex);
+
+	proxy_thread_join(manager->tid);
+}
+
+int32_t proxy_manager_run(proxy_manager_t *manager, proxy_manager_start_t start)
+{
+	struct sigaction old_action;
+	int32_t err;
+
+	err = proxy_manager_init(manager);
+	if (err < 0) {
+		return err;
+	}
+
+	err = proxy_manager_setup_signals(&old_action);
+	if (err < 0) {
+		goto done_destroy;
+	}
+
+	err = proxy_thread_create(&manager->tid, proxy_manager_start, manager);
+	if (err < 0) {
+		goto done_signal;
+	}
+
+	err = start(manager);
+
+	proxy_manager_terminate(manager);
+
+done_signal:
+	proxy_manager_restore_signals(&old_action);
+
+done_destroy:
+	proxy_manager_destroy(manager);
+
+	return err;
+}
+
+void proxy_manager_shutdown(proxy_manager_t *manager)
+{
+	proxy_mutex_lock(&manager->mutex);
+
+	manager->stop = true;
+	proxy_condition_signal(&manager->condition);
+
+	proxy_mutex_unlock(&manager->mutex);
+
+	/* Wake the thread if it was blocked in an I/O operation. */
+	proxy_thread_kill(manager->main_tid, SIGCONT);
+}
+
+int32_t proxy_manager_launch(proxy_manager_t *manager, proxy_worker_t *worker,
+			     proxy_worker_start_t start,
+			     proxy_worker_destroy_t destroy)
+{
+	int32_t err;
+
+	worker->manager = manager;
+	worker->start = start;
+	worker->destroy = destroy;
+	worker->stop = false;
+
+	proxy_worker_register(worker);
+
+	err = proxy_thread_create(&worker->tid, proxy_worker_start, worker);
+	if (err < 0) {
+		proxy_worker_deregister(worker);
+	}
+
+	return err;
+}