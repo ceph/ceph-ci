salieri11/ceph

qa: Convert and create tests for libcephfs fscrypt
Convert existing tests to use teuthology framework.
Create tests to test N>1 fscrypt clients

Fixes: https://tracker.ceph.com/issues/66577
Signed-off-by: Christopher Hoffman <choffman@redhat.com>

qa/tasks/cephfs/test_fscrypt.py
@@ -2,6 +2,7 @@ from io import StringIO
 from os.path import basename
 import random
 import string
+import time
 
 from logging import getLogger
 
@@ -89,42 +90,250 @@ class TestFSCryptRecovery(FSCryptTestCase):
         self.mount_a.run_shell_payload(f"cd {self.path} && stat {file}")
 
 class TestFSCryptRMW(FSCryptTestCase):
-    def test_fscrypt_overwrite_block_boundary():
+    CLIENTS_REQUIRED = 2
+    def setUp(self):
+        super().setUp()
+        self.mount_b.run_shell_payload(f"sudo fscrypt unlock --quiet --key=/tmp/key {self.path}")
+
+    def test_fscrypt_overwrite_block_boundary(self):
         """Test writing data with small, half write on previous block and trailing on new block"""
 
-        file = 'file.log'
+        file = f'{self.path}/file.log'
 
         size = 5529
         offset = 3379
         contents = 's' * size
-        self.mount_a.write_file(file, contents, offset)
+        self.mount_a.write_file(path=file, data=contents, offset=offset)
 
-        #s = write_fill(fd, 's', 5529, 6144)
-        sleep(10)
+        time.sleep(10)
 
         size = 4033
         offset = 4127
         contents = 't' * size
-        self.mount_a.write_file(file, contents, offset)
-        #s = write_fill(fd, 't', 4033, 6144)
+        self.mount_a.write_file(path=file, data=contents, offset=offset)
 
     def test_fscrypt_huge_hole(self):
         """Test writing data with huge hole, half write on previous block and trailing on new block"""
 
-        file = 'file.log'
+        file = f'{self.path}/file.log'
 
         size = 4096
         offset = 107374180352
         contents = 's' * size
-        self.mount_a.write_file(file, contents, offset)
-        #s = write_fill(fd, 's', 4096, 107374182400)
-        sleep(10)
+        self.mount_a.write_file(path=file, data=contents, offset=offset)
+        time.sleep(10)
         
         size = 8
         offset = 12
         contents = 't' * size
-        self.mount_a.write_file(file, contents, offset)
+        self.mount_a.write_file(path=file, data=contents, offset=offset)
         #s = write_fill(fd, 't', 8, 16)
+
+    def test_fscrypt_med_hole_write_boundary(self):
+        """Test writing data past many holes on offset 0 of block"""
+
+        file = f'{self.path}/file.log'
+
+        #reproducing sys calls after ffsb bench has started
+        size = 3192
+        offset = 60653568
+        contents = 's' * size
+        self.mount_a.write_file(path=file, data=contents, offset=offset)
+
+    def test_fscrypt_simple_rmw(self):
+        """ Test simple rmw"""
+
+        file = f'{self.path}/file.log'
+
+        size = 32
+        offset = 0
+        contents = 's' * size
+        self.mount_a.write_file(path=file, data=contents, offset=offset)
+
+        size = 8
+        offset = 8
+        contents = 't' * size
+        self.mount_a.write_file(path=file, data=contents, offset=offset)
+
+        src_hash = self.mount_a.dir_checksum(path=file)
+        dest_hash = self.mount_b.dir_checksum(path=file)
+
+        if src_hash != dest_hash:
+            raise ValueError
+
+    def test_fscrypt_truncate_overwrite(self):
+        """ Test copy smaller file -> larger file gets new file size"""
+
+        file1 = f'{self.path}/file1.log'
+        file2 = f'{self.path}/file2.log'
+        expected_size = 1024
+
+        self.mount_a.touch(file1)
+        self.mount_a.touch(file2)
+
+        self.mount_a.truncate(file1, 1048576)
+        self.mount_a.truncate(file2, 1024)
+
+        #simulate copy file2 -> file1
+        self.mount_a.copy_file_range(file2, file1, 9223372035781033984)
+        actual_size = self.mount_a.stat(file1)['st_size']
+
+        if actual_size != expected_size:
+            raise ValueError
+
+    def test_fscrypt_truncate_path(self):
+        """ Test overwrite/cp displays effective_size and not real size"""
+
+        file = f'{self.path}/file.log'
+        expected_size = 68686
+
+        #fstest create test1 0644;
+        self.mount_a.touch(file1)
+
+        #fstest truncate test1 68686;
+        self.mount_a.truncate(file, expected_size)
+
+        #fstest stat test1 size
+        if self.mount_a.lstat(file)['st_size'] != expected_size:
+            raise ValueError
+        #stat above command returns 69632 instead of truncated value.
+
+    def test_fscrypt_lchown_symlink(self):
+        """ Test lchown to ensure target is set"""
+
+        file1 = f'{self.path}/file1.log'
+
+        self.mount_a.touch(file1)
+
+        #fstest symlink file1 symlink1
+        file2 = f'{self.path}/symlink'
+        self.mount_a.symlink(file1, file2)
+
+        #fstest lchown symlink1 135 579
+        self.mount_a.lchown(file2, 1000, 1000)
+
+        # ls -l
+        #-rw-r--r--. 1 root root  0 Apr 22 18:11 file1
+        #lrwxrwxrwx. 1  135  579 46 Apr 22 18:11 symlink1 -> ''$'\266\310''%'$'\005''W'$'\335''.'$'\355\211''kblD'$'\300''gq'$'\002\236\367''3'$'\255\201\001''Z6;'$'\221''&'$'\216\331\177''Q'
+        ###if os.readlink(file2) != file1:
+           ### raise Exception
+
+    def test_fscrypt_900mhole_100mwrite(self):
+        """ Test 900m hole 100m data write"""
+
+        size = 100
+        offset = 900
+
+        files=[f'{self.path}/kfile.log', f'{self.path}/fuse_file.log']
+        KERNEL_INDEX = 0
+        FUSE_INDEX = 1
+
+        self.mount_a.write_n_mb(files[KERNEL_INDEX], size, seek=offset)
+        src_hash = self.mount_a.dir_checksum(path=files[KERNEL_INDEX])
+        dest_hash = self.mount_b.dir_checksum(path=files[KERNEL_INDEX])
+
+        if src_hash != dest_hash:
+            raise ValueError
+
+        self.mount_b.write_n_mb(files[FUSE_INDEX], size, seek=offset)
+        src_hash = self.mount_b.dir_checksum(path=files[FUSE_INDEX])
+        dest_hash = self.mount_a.dir_checksum(path=files[FUSE_INDEX])
+
+        if src_hash != dest_hash:
+            raise ValueError
+
+    def test_fscrypt_1gwrite_400m600mwrite(self):
+        """ Test 200M overwrite of 1G file"""
+
+        file=f'{self.path}/file.log'
+
+        self.mount_a.write_n_mb(file, 1000)
+        self.mount_b.write_n_mb(file, 200, seek=400)
+        client1_hash = self.mount_a.dir_checksum(path=file)
+        client2_hash = self.mount_b.dir_checksum(path=file)
+
+        if client1_hash != client2_hash:
+            raise ValueError
+
+    def test_fscrypt_truncate_ladder(self):
+        """ Test truncate down from 1GB"""
+
+        file = f'{self.path}/file.log'
+        expected_sizes = [1024, 900, 500, 1]
+
+        # define the truncate side and the read side
+        tside = self.mount_a
+        rside = self.mount_b
+
+        self.tside.touch(file)
+
+        for expected_size in expected_sizes:
+            tside.truncate(file, expected_size)
+            tside_size = tside.stat(file)['st_size']
+            rside_size = tside.stat(file)['st_size']
+            if tside_size != rside_size:
+                raise ValueError
+
+            #swap which client does the truncate
+            tside, rside = rside, tside
+
+    def strided_tests(self, fscrypt_block_size, write_size, num_writes, shared_file, fill):
+        wside = self.mount_a
+        rside = self.mount_b
+
+        contents = fill * fscrypt_block_size
+
+        for i in range(num_writes):
+            offset = i * write_size
+            end_offset = offset + write_size
+            strided_write = contents[offset:end_offset]
+            s_size = len(strided_write)
+            print(f"=============== {offset} to - {end_offset} size: {s_size} ==============")
+            wside.write_file(path=shared_file, data=strided_write, offset=offset, sync=True)
+            wside, rside = rside, wside
+
+        shared_contents1 = wside.read_file(shared_file)
+        shared_contents2 = rside.read_file(shared_file)
+
+        if shared_contents1 != shared_contents2:
+            raise ValueError
+
+        if contents != shared_contents1:
+            print(f"================= {contents} \n vs \n {shared_contents1}")
+            raise ValueError
+
+    def test_fscrypt_strided_small(self):
+        """ Test strided i/o within a single fscrypt block"""
+
+        fscrypt_block_size = 4096
+        write_size = 256
+        num_writes = 16
+        shared_file = f'{self.path}/file.log'
+        fill = 's'
+
+        self.strided_tests(fscrypt_block_size, write_size, num_writes, shared_file, fill)
+
+    def test_fscrypt_strided_regular_write(self):
+        """ Test aligned strided i/o on fscrypt block"""
+
+        fscrypt_block_size = 4096
+        write_size = fscrypt_block_size
+        num_writes = 16
+        shared_file = f'{self.path}/file.log'
+        fill = 's'
+
+        self.strided_tests(fscrypt_block_size, write_size, num_writes, shared_file, fill)
+
+    def test_unaligned_strided_write(self):
+        """ Test unaligned strided i/o on fscrypt block"""
+
+        fscrypt_block_size = 4096
+        write_size = 4000
+        num_writes = 16
+        shared_file = f'{self.path}/file.log'
+        fill = 's'
+
+        self.strided_tests(fscrypt_block_size, write_size, num_writes, shared_file, fill)
 
 class TestFSCryptXFS(XFSTestsDev):
 

ceph/ceph-ci

qa/tasks/cephfs/test_fscrypt.py
@@ -2,6 +2,7 @@ from io import StringIO
 from os.path import basename
 import random
 import string
+import time
 
 from logging import getLogger
 
@@ -91,42 +92,249 @@ class TestFSCryptRecovery(FSCryptTestCase):
         self.mount_a.run_shell_payload(f"cd {self.path} && stat {file}")
 
 class TestFSCryptRMW(FSCryptTestCase):
-    def test_fscrypt_overwrite_block_boundary():
+    CLIENTS_REQUIRED = 2
+    def setUp(self):
+        super().setUp()
+        self.mount_b.run_shell_payload(f"sudo fscrypt unlock --quiet --key=/tmp/key {self.path}")
+
+    def test_fscrypt_overwrite_block_boundary(self):
         """Test writing data with small, half write on previous block and trailing on new block"""
 
-        file = 'file.log'
+        file = f'{self.path}/file.log'
 
         size = 5529
         offset = 3379
         contents = 's' * size
-        self.mount_a.write_file(file, contents, offset)
+        self.mount_a.write_file_ex(path=file, bs=1, data=contents, offset=offset)
 
-        #s = write_fill(fd, 's', 5529, 6144)
-        sleep(10)
+        time.sleep(10)
 
         size = 4033
         offset = 4127
         contents = 't' * size
-        self.mount_a.write_file(file, contents, offset)
-        #s = write_fill(fd, 't', 4033, 6144)
+        self.mount_a.write_file_ex(path=file, bs=1, data=contents, offset=offset)
 
     def test_fscrypt_huge_hole(self):
         """Test writing data with huge hole, half write on previous block and trailing on new block"""
 
-        file = 'file.log'
+        file = f'{self.path}/file.log'
 
         size = 4096
         offset = 107374180352
         contents = 's' * size
-        self.mount_a.write_file(file, contents, offset)
-        #s = write_fill(fd, 's', 4096, 107374182400)
-        sleep(10)
+        self.mount_a.write_file_ex(path=file, bs=1, data=contents, offset=offset)
+        time.sleep(10)
 
         size = 8
         offset = 12
         contents = 't' * size
-        self.mount_a.write_file(file, contents, offset)
-        #s = write_fill(fd, 't', 8, 16)
+        self.mount_a.write_file_ex(path=file, bs=1, data=contents, offset=offset)
+
+    def test_fscrypt_med_hole_write_boundary(self):
+        """Test writing data past many holes on offset 0 of block"""
+
+        file = f'{self.path}/file.log'
+
+        #reproducing sys calls after ffsb bench has started
+        size = 3192
+        offset = 60653568
+        contents = 's' * size
+        self.mount_a.write_file_ex(path=file, bs=1, data=contents, offset=offset)
+
+    def test_fscrypt_simple_rmw(self):
+        """ Test simple rmw"""
+
+        file = f'{self.path}/file.log'
+
+        size = 32
+        offset = 0
+        contents = 's' * size
+        self.mount_a.write_file_ex(path=file, bs=1, data=contents, offset=offset)
+
+        size = 8
+        offset = 8
+        contents = 't' * size
+        self.mount_a.write_file_ex(path=file, bs=1, data=contents, offset=offset)
+
+        src_hash = self.mount_a.dir_checksum(path=file)
+        dest_hash = self.mount_b.dir_checksum(path=file)
+
+        if src_hash != dest_hash:
+            raise ValueError
+
+    def test_fscrypt_truncate_overwrite(self):
+        """ Test copy smaller file -> larger file gets new file size"""
+
+        file1 = f'{self.path}/file1.log'
+        file2 = f'{self.path}/file2.log'
+        expected_size = 1024
+
+        self.mount_a.touch(file1)
+        self.mount_a.touch(file2)
+
+        self.mount_a.truncate(file1, 1048576)
+        self.mount_a.truncate(file2, 1024)
+
+        #simulate copy file2 -> file1
+        self.mount_a.copy_file_range(file2, file1, 9223372035781033984)
+        actual_size = self.mount_a.stat(file1)['st_size']
+
+        if actual_size != expected_size:
+            raise ValueError
+
+    def test_fscrypt_truncate_path(self):
+        """ Test overwrite/cp displays effective_size and not real size"""
+
+        file = f'{self.path}/file.log'
+        expected_size = 68686
+
+        #fstest create test1 0644;
+        self.mount_a.touch_os(file)
+
+        #fstest truncate test1 68686;
+        self.mount_a.truncate(file, expected_size)
+
+        #fstest stat test1 size
+        if self.mount_a.lstat(file)['st_size'] != expected_size:
+            raise ValueError
+        #stat above command returns 69632 instead of truncated value.
+
+    def test_fscrypt_lchown_symlink(self):
+        """ Test lchown to ensure target is set"""
+
+        file1 = f'{self.path}/file1.log'
+
+        self.mount_a.touch(file1)
+
+        #fstest symlink file1 symlink1
+        file2 = f'{self.path}/symlink'
+        self.mount_a.symlink(file1, file2)
+
+        #fstest lchown symlink1 135 579
+        self.mount_a.lchown(file2, 1000, 1000)
+
+        # ls -l
+        #-rw-r--r--. 1 root root  0 Apr 22 18:11 file1
+        #lrwxrwxrwx. 1  135  579 46 Apr 22 18:11 symlink1 -> ''$'\266\310''%'$'\005''W'$'\335''.'$'\355\211''kblD'$'\300''gq'$'\002\236\367''3'$'\255\201\001''Z6;'$'\221''&'$'\216\331\177''Q'
+        ###if os.readlink(file2) != file1:
+           ### raise Exception
+
+    def test_fscrypt_900mhole_100mwrite(self):
+        """ Test 900m hole 100m data write"""
+
+        size = 100
+        offset = 900
+
+        files=[f'{self.path}/kfile.log', f'{self.path}/fuse_file.log']
+        KERNEL_INDEX = 0
+        FUSE_INDEX = 1
+
+        self.mount_a.write_n_mb(files[KERNEL_INDEX], size, seek=offset)
+        src_hash = self.mount_a.dir_checksum(path=files[KERNEL_INDEX])
+        dest_hash = self.mount_b.dir_checksum(path=files[KERNEL_INDEX])
+
+        if src_hash != dest_hash:
+            raise ValueError
+
+        self.mount_b.write_n_mb(files[FUSE_INDEX], size, seek=offset)
+        src_hash = self.mount_b.dir_checksum(path=files[FUSE_INDEX])
+        dest_hash = self.mount_a.dir_checksum(path=files[FUSE_INDEX])
+
+        if src_hash != dest_hash:
+            raise ValueError
+
+    def test_fscrypt_1gwrite_400m600mwrite(self):
+        """ Test 200M overwrite of 1G file"""
+
+        file=f'{self.path}/file.log'
+
+        self.mount_a.write_n_mb(file, 1000)
+        self.mount_b.write_n_mb(file, 200, seek=400)
+        client1_hash = self.mount_a.dir_checksum(path=file)
+        client2_hash = self.mount_b.dir_checksum(path=file)
+
+        if client1_hash != client2_hash:
+            raise ValueError
+
+    def test_fscrypt_truncate_ladder(self):
+        """ Test truncate down from 1GB"""
+
+        file = f'{self.path}/file.log'
+        expected_sizes = [1024, 900, 500, 1]
+
+        # define the truncate side and the read side
+        tside = self.mount_a
+        rside = self.mount_b
+
+        tside.touch(file)
+
+        for expected_size in expected_sizes:
+            tside.truncate(file, expected_size)
+            tside_size = tside.stat(file)['st_size']
+            rside_size = rside.stat(file)['st_size']
+            if tside_size != rside_size:
+                raise ValueError
+
+            #swap which client does the truncate
+            tside, rside = rside, tside
+
+    def strided_tests(self, fscrypt_block_size, write_size, num_writes, shared_file, fill):
+        wside = self.mount_a
+        rside = self.mount_b
+
+        contents = fill * write_size * num_writes
+
+        for i in range(num_writes):
+            offset = i * write_size
+            end_offset = offset + write_size
+            strided_write = contents[offset:end_offset]
+            s_size = len(strided_write)
+            print(f"=============== {offset} to - {end_offset} size: {s_size} ==============")
+            wside.write_file_ex(path=shared_file, data=strided_write, bs=1, offset=offset, sync=True)
+            wside, rside = rside, wside
+
+        shared_contents1 = wside.read_file(shared_file)
+        shared_contents2 = rside.read_file(shared_file)
+
+        if shared_contents1 != shared_contents2:
+            raise ValueError
+
+        if contents != shared_contents1:
+            print(f"================= {contents} \n vs \n {shared_contents1}")
+            raise ValueError
+
+    def test_fscrypt_strided_small(self):
+        """ Test strided i/o within a single fscrypt block"""
+
+        fscrypt_block_size = 4096
+        write_size = 256
+        num_writes = 16
+        shared_file = f'{self.path}/file.log'
+        fill = 's'
+
+        self.strided_tests(fscrypt_block_size, write_size, num_writes, shared_file, fill)
+
+    def test_fscrypt_strided_regular_write(self):
+        """ Test aligned strided i/o on fscrypt block"""
+
+        fscrypt_block_size = 4096
+        write_size = fscrypt_block_size
+        num_writes = 16
+        shared_file = f'{self.path}/file.log'
+        fill = 's'
+
+        self.strided_tests(fscrypt_block_size, write_size, num_writes, shared_file, fill)
+
+    def test_unaligned_strided_write(self):
+        """ Test unaligned strided i/o on fscrypt block"""
+
+        fscrypt_block_size = 4096
+        write_size = 4000
+        num_writes = 16
+        shared_file = f'{self.path}/file.log'
+        fill = 's'
+
+        self.strided_tests(fscrypt_block_size, write_size, num_writes, shared_file, fill)
 
 class TestFSCryptXFS(XFSTestsDev):
 