273a5b0dc75cd95ed41875272b7bc236a7bbd5d5
Adam King <adking@redhat.com>
2025-01-23 13:58:33 -0500

    cephadm: add LimitCORE=infinity to ceph unit file template
    To stop from coredumpctl from truncating the core dumps
    which makes them much less useful
    
    Signed-off-by: Adam King <adking@redhat.com>

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -1193,7 +1193,11 @@ def deploy_daemon_units(
         DaemonSubIdentity.must(sc.identity) for sc in sidecars or []
     ]
     systemd_unit.update_files(
-        ctx, ident, init_container_ids=ic_ids, sidecar_ids=sc_ids
+        ctx,
+        ident,
+        init_container_ids=ic_ids,
+        sidecar_ids=sc_ids,
+        limit_core_infinity=ctx.limit_core_infinity,
     )
     call_throws(ctx, ['systemctl', 'daemon-reload'])
 
@@ -3335,7 +3339,7 @@ def command_unit_install(ctx):
     if not getattr(ctx, 'name', None):
         raise Error('daemon name required')
     ident = DaemonIdentity.from_context(ctx)
-    systemd_unit.update_files(ctx, ident)
+    systemd_unit.update_files(ctx, ident, limit_core_infinity=ctx.limit_core_infinity)
     call_throws(ctx, ['systemctl', 'daemon-reload'])
     return 0
 
@@ -3362,10 +3366,10 @@ def command_unit(ctx):
 def command_set_coredump_overrides(ctx: CephadmContext) -> None:
     if not ctx.cleanup:
         set_coredump_overrides(ctx, ctx.fsid, ctx.coredump_max_size)
-        systemd_unit.update_base_ceph_unit_file(ctx, ctx.fsid)
+        systemd_unit.update_base_ceph_unit_file(ctx, ctx.fsid, limit_core_infinity=True)
     else:
         remove_coredump_overrides(ctx, ctx.fsid)
-        systemd_unit.update_base_ceph_unit_file(ctx, ctx.fsid)
+        systemd_unit.update_base_ceph_unit_file(ctx, ctx.fsid, limit_core_infinity=False)
 
 ##################################
 
@@ -4814,6 +4818,12 @@ def _add_deploy_parser_args(
         default=[],
         help='Additional entrypoint arguments to apply to deamon'
     )
+    parser_deploy.add_argument(
+        '--limit-core-infinity',
+        action='store_true',
+        default=False,
+        help='Set LimitCORE=infinity in ceph unit files'
+    )
 
 
 def _get_parser():
@@ -4973,6 +4983,12 @@ def _get_parser():
         action='store_true',
         default=False,
         help='Do not run containers with --cgroups=split (currently only relevant when using podman)')
+    parser_adopt.add_argument(
+        '--limit-core-infinity',
+        action='store_true',
+        default=False,
+        help='Set LimitCORE=infinity in generated ceph unit files'
+    )
 
     parser_rm_daemon = subparsers.add_parser(
         'rm-daemon', help='remove daemon instance')
@@ -5151,6 +5167,11 @@ def _get_parser():
         '--name', '-n',
         required=True,
         help='daemon name (type.id)')
+    parser_unit.add_argument(
+        '--limit-core-infinity',
+        action='store_true',
+        help='Set LimitCORE=infinity in ceph unit files'
+    )
 
     parser_logs = subparsers.add_parser(
         'logs', help='print journald logs for a daemon container')
@@ -5356,6 +5377,11 @@ def _get_parser():
     parser_bootstrap.add_argument(
         '--custom-prometheus-alerts',
         help='provide a file with custom prometheus alerts')
+    parser_bootstrap.add_argument(
+        '--limit-core-infinity',
+        action='store_true',
+        help='Set LimitCORE=infinity in ceph unit files'
+    )
 
     parser_deploy = subparsers.add_parser(
         'deploy', help='deploy a daemon')
