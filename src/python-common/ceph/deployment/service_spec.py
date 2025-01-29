8936f0fb825f62bdad97a4272ea76662a4dbaa05
Omid Yoosefi <omidyoosefi@ibm.com>
2025-01-29 15:48:52 -0500

    pybind/mgr/cephadm: allow setting custom monitoring_port for nfs
    ganesha config allows this, so allow users to set their own custom
    ports in case they wish to do so.
    
    Signed-off-by: Omid Yoosefi <omidyoosefi@ibm.com>
    (cherry picked from commit 7af22ba00080d2dafcb88f5a182074c6593675d6)
    
    Resolves: rhbz#2346194

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1129,6 +1129,7 @@ class NFSServiceSpec(ServiceSpec):
                  config: Optional[Dict[str, str]] = None,
                  networks: Optional[List[str]] = None,
                  port: Optional[int] = None,
+                 monitoring_port: Optional[int] = None,
                  virtual_ip: Optional[str] = None,
                  enable_nlm: bool = False,
                  enable_haproxy_protocol: bool = False,
@@ -1145,6 +1146,7 @@ class NFSServiceSpec(ServiceSpec):
             extra_entrypoint_args=extra_entrypoint_args, custom_configs=custom_configs)
 
         self.port = port
+        self.monitoring_port = monitoring_port
         self.virtual_ip = virtual_ip
         self.enable_haproxy_protocol = enable_haproxy_protocol
         self.idmap_conf = idmap_conf
