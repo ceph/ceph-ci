bill-scales/ceph

test: ceph_test_rados_io_sequence append sequences
Sequence 11 - sequential appends of fixed size
Sequence 12 - sequentiial overwrite+append of fixed size and different offsets

Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

src/common/io_exerciser/IoSequence.h
@@ -43,6 +43,8 @@ namespace ceph {
       SEQUENCE_SEQ8,
       SEQUENCE_SEQ9,
       SEQUENCE_SEQ10,
+      SEQUENCE_SEQ11,
+      SEQUENCE_SEQ12,
 
       SEQUENCE_END,
       SEQUENCE_BEGIN = SEQUENCE_SEQ0
@@ -229,6 +231,35 @@ namespace ceph {
 
     public:
       Seq9(std::pair<int,int> obj_size_range, int seed);
+
+      Sequence get_id() const override;
+      std::string get_name() const override;
+      std::unique_ptr<IoOp> _next() override;
+    };
+
+    class Seq11: public IoSequence {
+    private:
+      uint64_t count;
+      bool doneread = true;
+      bool donebarrier = false;
+
+    public:
+      Seq11(std::pair<int,int> obj_size_range, int seed);
+
+      Sequence get_id() const override;
+      std::string get_name() const override;
+      std::unique_ptr<IoOp> _next() override;
+    };
+
+    class Seq12: public IoSequence {
+    private:
+      uint64_t count;
+      uint64_t overlap;
+      bool doneread = true;
+      bool donebarrier = false;
+
+    public:
+      Seq12(std::pair<int,int> obj_size_range, int seed);
 
       Sequence get_id() const override;
       std::string get_name() const override;

ceph/ceph-ci

src/common/io_exerciser/IoSequence.h
@@ -42,6 +42,8 @@ enum class Sequence {
   SEQUENCE_SEQ8,
   SEQUENCE_SEQ9,
   SEQUENCE_SEQ10,
+  SEQUENCE_SEQ11,
+  SEQUENCE_SEQ12,
 
   SEQUENCE_END,
   SEQUENCE_BEGIN = SEQUENCE_SEQ0
@@ -227,6 +229,35 @@ class Seq9 : public IoSequence {
 
  public:
   Seq9(std::pair<int, int> obj_size_range, int seed);
+
+      Sequence get_id() const override;
+      std::string get_name() const override;
+      std::unique_ptr<IoOp> _next() override;
+    };
+
+class Seq11 : public IoSequence {
+ private:
+  uint64_t count;
+  bool doneread = true;
+  bool donebarrier = false;
+
+ public:
+  Seq11(std::pair<int, int> obj_size_range, int seed);
+
+  Sequence get_id() const override;
+  std::string get_name() const override;
+  std::unique_ptr<IoOp> _next() override;
+};
+
+class Seq12 : public IoSequence {
+ private:
+  uint64_t count;
+  uint64_t overlap;
+  bool doneread = true;
+  bool donebarrier = false;
+
+ public:
+  Seq12(std::pair<int, int> obj_size_range, int seed);
 
   Sequence get_id() const override;
   std::string get_name() const override;