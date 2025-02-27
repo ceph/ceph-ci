ceph/ceph

mgr/dashboard: create smb cluster
Signed-off-by: Dnyaneshwari Talwekar <dtalwekar@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/api/smb.service.spec.ts
@@ -28,4 +28,10 @@ describe('SmbService', () => {
     const req = httpTesting.expectOne('api/smb/cluster');
     expect(req.request.method).toBe('GET');
   });
+
+  it('should call create', () => {
+    service.create('test').subscribe();
+    const req = httpTesting.expectOne('api/smb/cluster');
+    expect(req.request.method).toBe('POST');
+  });
 });

src/pybind/mgr/dashboard/frontend/src/app/shared/api/smb.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 import { SMBCluster } from '~/app/ceph/smb/smb.model';
 
@@ -9,10 +9,20 @@ import { SMBCluster } from '~/app/ceph/smb/smb.model';
 })
 export class SmbService {
   baseURL = 'api/smb';
+  private modalDataSubject = new Subject<any>();
+  modalData$ = this.modalDataSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
+  passData(data: any) {
+    this.modalDataSubject.next(data);
+  }
+
   listClusters(): Observable<SMBCluster[]> {
     return this.http.get<SMBCluster[]>(`${this.baseURL}/cluster`);
+  }
+
+  create(requestModel: any) {
+    return this.http.post(`${this.baseURL}/cluster`, requestModel);
   }
 }

ceph/ceph-ci

Fixes: https://tracker.ceph.com/issues/69156

src/pybind/mgr/dashboard/frontend/src/app/shared/api/smb.service.spec.ts
@@ -29,6 +29,12 @@ describe('SmbService', () => {
     expect(req.request.method).toBe('GET');
   });
 
+  it('should call create', () => {
+    service.createCluster('test').subscribe();
+    const req = httpTesting.expectOne('api/smb/cluster');
+    expect(req.request.method).toBe('POST');
+  });
+
   it('should call remove', () => {
     service.removeCluster('cluster_1').subscribe();
     const req = httpTesting.expectOne('api/smb/cluster/cluster_1');

src/pybind/mgr/dashboard/frontend/src/app/shared/api/smb.service.ts
@@ -1,19 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
-import { SMBCluster } from '~/app/ceph/smb/smb.model';
+import { DomainSettings, SMBCluster } from '~/app/ceph/smb/smb.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmbService {
   baseURL = 'api/smb';
+  private modalDataSubject = new Subject<DomainSettings>();
+  modalData$ = this.modalDataSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
+  passData(data: DomainSettings) {
+    this.modalDataSubject.next(data);
+  }
+
   listClusters(): Observable<SMBCluster[]> {
     return this.http.get<SMBCluster[]>(`${this.baseURL}/cluster`);
+  }
+
+  createCluster(requestModel: any) {
+    return this.http.post(`${this.baseURL}/cluster`, requestModel);
   }
 
   removeCluster(clusterId: string) {