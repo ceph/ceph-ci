ceph/ceph

rgw/rgw_admin.cc : Make restore attrs readable in admin cli
The restore attrs directly stored at the backend which make it difficult to understand
via `radosgw-admin object stat` cli. Make it more human readable form.

Signed-off-by: Jiffin Tony Thottan <thottanjiffin@gmail.com>

src/rgw/rgw_sal.cc
@@ -415,4 +415,38 @@ int Object::range_to_ofs(uint64_t obj_size, int64_t &ofs, int64_t &end)
   }
   return 0;
 }
+
+
+std::string rgw_restore_status_dump(rgw::sal::RGWRestoreStatus status)
+{
+  switch (status)
+  {
+  case RGWRestoreStatus::None:
+    return "None";
+  case RGWRestoreStatus::RestoreAlreadyInProgress:
+    return "RestoreAlreadyInProgress";
+  case RGWRestoreStatus::CloudRestored:
+    return "CloudRestored";
+  case RGWRestoreStatus::RestoreFailed:
+    return "RestoreFailed";
+  default:
+    return "";
+  }
+}
+
+std::string rgw_restore_type_dump(rgw::sal::RGWRestoreType type)
+{
+  switch (type)
+  {
+  case RGWRestoreType::None:
+    return "None";
+  case RGWRestoreType::Temporary:
+    return "Temporary";
+  case RGWRestoreType::Permanent:
+    return "Permanent";
+  default:
+    return "";
+  }
+}
+
 } // namespace rgw::sal

src/rgw/rgw_sal.h
@@ -160,6 +160,7 @@ enum RGWRestoreStatus : uint8_t {
   RestoreFailed = 3
 };
 
+std::string rgw_restore_status_dump(rgw::sal::RGWRestoreStatus status);
 
 enum class RGWRestoreType : uint8_t {
   None = 0,
@@ -167,6 +168,7 @@ enum class RGWRestoreType : uint8_t {
   Permanent = 2
 };
 
+std::string rgw_restore_type_dump(rgw::sal::RGWRestoreType type);
 
 // a simple streaming data processing abstraction
 /**

ceph/ceph-ci

src/rgw/rgw_sal.cc
@@ -415,4 +415,38 @@ int Object::range_to_ofs(uint64_t obj_size, int64_t &ofs, int64_t &end)
   }
   return 0;
 }
+
+
+std::string_view rgw_restore_status_dump(rgw::sal::RGWRestoreStatus status)
+{
+  switch (status)
+  {
+  case RGWRestoreStatus::None:
+    return "None";
+  case RGWRestoreStatus::RestoreAlreadyInProgress:
+    return "RestoreAlreadyInProgress";
+  case RGWRestoreStatus::CloudRestored:
+    return "CloudRestored";
+  case RGWRestoreStatus::RestoreFailed:
+    return "RestoreFailed";
+  default:
+    return "";
+  }
+}
+
+std::string_view rgw_restore_type_dump(rgw::sal::RGWRestoreType type)
+{
+  switch (type)
+  {
+  case RGWRestoreType::None:
+    return "None";
+  case RGWRestoreType::Temporary:
+    return "Temporary";
+  case RGWRestoreType::Permanent:
+    return "Permanent";
+  default:
+    return "";
+  }
+}
+
 } // namespace rgw::sal

src/rgw/rgw_sal.h
@@ -162,6 +162,7 @@ enum RGWRestoreStatus : uint8_t {
   RestoreFailed = 3
 };
 
+std::string_view rgw_restore_status_dump(rgw::sal::RGWRestoreStatus status);
 
 enum class RGWRestoreType : uint8_t {
   None = 0,
@@ -169,6 +170,7 @@ enum class RGWRestoreType : uint8_t {
   Permanent = 2
 };
 
+std::string_view rgw_restore_type_dump(rgw::sal::RGWRestoreType type);
 
 // a simple streaming data processing abstraction
 /**