ceph/ceph

mgr/dashboard: add actions to create, edit and delete smb join-auth and usersgroups resources
Add join-auth and usersgroups resources management and improve the way to select those from the smb cluster form using a dropdown.
Add option to navigate to join-auth/usersgroups resource create form from smb form
Add some additional fixes left over from previous work, such as adding helper texts or adding missing smb cluster form fields

Fixes: https://tracker.ceph.com/issues/69483
Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/app-routing.module.ts
@@ -54,6 +54,8 @@ import { MultiClusterDetailsComponent } from './ceph/cluster/multi-cluster/multi
 import { SmbClusterFormComponent } from './ceph/smb/smb-cluster-form/smb-cluster-form.component';
 import { SmbTabsComponent } from './ceph/smb/smb-tabs/smb-tabs.component';
 import { SmbShareFormComponent } from './ceph/smb/smb-share-form/smb-share-form.component';
+import { SmbJoinAuthFormComponent } from './ceph/smb/smb-join-auth-form/smb-join-auth-form.component';
+import { SmbUsersgroupsFormComponent } from './ceph/smb/smb-usersgroups-form/smb-usersgroups-form.component';
 
 @Injectable()
 export class PerformanceCounterBreadcrumbsResolver extends BreadcrumbsResolver {
@@ -457,6 +459,26 @@ const routes: Routes = [
                 path: `share/${URLVerbs.CREATE}/:clusterId`,
                 component: SmbShareFormComponent,
                 data: { breadcrumbs: ActionLabels.CREATE }
+              },
+              {
+                path: `joinauth/${URLVerbs.CREATE}`,
+                component: SmbJoinAuthFormComponent,
+                data: { breadcrumbs: ActionLabels.CREATE }
+              },
+              {
+                path: `joinauth/${URLVerbs.EDIT}/:authId`,
+                component: SmbJoinAuthFormComponent,
+                data: { breadcrumbs: ActionLabels.EDIT }
+              },
+              {
+                path: `usersgroups/${URLVerbs.CREATE}`,
+                component: SmbUsersgroupsFormComponent,
+                data: { breadcrumbs: ActionLabels.CREATE }
+              },
+              {
+                path: `usersgroups/${URLVerbs.EDIT}/:usersGroupsId`,
+                component: SmbUsersgroupsFormComponent,
+                data: { breadcrumbs: ActionLabels.EDIT }
               }
             ]
           }

src/pybind/mgr/dashboard/frontend/src/app/ceph/smb/smb-join-auth-list/smb-join-auth-list.component.ts
@@ -9,11 +9,22 @@ import { CdTableFetchDataContext } from '~/app/shared/models/cd-table-fetch-data
 import { Permission } from '~/app/shared/models/permissions';
 import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
 import { SMBJoinAuth } from '../smb.model';
+import { Router } from '@angular/router';
+import { Icons } from '~/app/shared/enum/icons.enum';
+import { CdTableSelection } from '~/app/shared/models/cd-table-selection';
+import { URLBuilderService } from '~/app/shared/services/url-builder.service';
+import { CriticalConfirmationModalComponent } from '~/app/shared/components/critical-confirmation-modal/critical-confirmation-modal.component';
+import { ModalCdsService } from '~/app/shared/services/modal-cds.service';
+import { FinishedTask } from '~/app/shared/models/finished-task';
+import { TaskWrapperService } from '~/app/shared/services/task-wrapper.service';
+
+export const JOINAUTH_URL = '/cephfs/smb/joinauth';
 
 @Component({
   selector: 'cd-smb-join-auth-list',
   templateUrl: './smb-join-auth-list.component.html',
-  styleUrls: ['./smb-join-auth-list.component.scss']
+  styleUrls: ['./smb-join-auth-list.component.scss'],
+  providers: [{ provide: URLBuilderService, useValue: new URLBuilderService(JOINAUTH_URL) }]
 })
 export class SmbJoinAuthListComponent implements OnInit {
   columns: CdTableColumn[];
@@ -23,11 +34,16 @@ export class SmbJoinAuthListComponent implements OnInit {
 
   joinAuth$: Observable<SMBJoinAuth[]>;
   subject$ = new BehaviorSubject<SMBJoinAuth[]>([]);
+  selection: CdTableSelection = new CdTableSelection();
 
   constructor(
+    private router: Router,
+    private urlBuilder: URLBuilderService,
     private authStorageService: AuthStorageService,
     public actionLabels: ActionLabelsI18n,
-    private smbService: SmbService
+    private smbService: SmbService,
+    private modalService: ModalCdsService,
+    private taskWrapper: TaskWrapperService
   ) {
     this.permission = this.authStorageService.getPermissions().smb;
   }
@@ -35,7 +51,7 @@ export class SmbJoinAuthListComponent implements OnInit {
   ngOnInit() {
     this.columns = [
       {
-        name: $localize`ID`,
+        name: $localize`Name`,
         prop: 'auth_id',
         flexGrow: 2
       },
@@ -45,9 +61,32 @@ export class SmbJoinAuthListComponent implements OnInit {
         flexGrow: 2
       },
       {
-        name: $localize`Linked to Cluster`,
+        name: $localize`Linked to cluster`,
         prop: 'linked_to_cluster',
         flexGrow: 2
+      }
+    ];
+
+    this.tableActions = [
+      {
+        name: `${this.actionLabels.CREATE} AD`,
+        permission: 'create',
+        icon: Icons.add,
+        click: () => this.router.navigate([this.urlBuilder.getCreate()]),
+        canBePrimary: (selection: CdTableSelection) => !selection.hasSelection
+      },
+      {
+        name: this.actionLabels.EDIT,
+        permission: 'update',
+        icon: Icons.edit,
+        click: () =>
+          this.router.navigate([this.urlBuilder.getEdit(String(this.selection.first().auth_id))])
+      },
+      {
+        name: this.actionLabels.DELETE,
+        permission: 'update',
+        icon: Icons.destroy,
+        click: () => this.openDeleteModal()
       }
     ];
 
@@ -65,5 +104,25 @@ export class SmbJoinAuthListComponent implements OnInit {
 
   loadJoinAuth() {
     this.subject$.next([]);
+  }
+
+  openDeleteModal() {
+    const authId = this.selection.first().auth_id;
+
+    this.modalService.show(CriticalConfirmationModalComponent, {
+      itemDescription: $localize`Active directory access resource`,
+      itemNames: [authId],
+      submitActionObservable: () =>
+        this.taskWrapper.wrapTaskAroundCall({
+          task: new FinishedTask('smb/joinauth/remove', {
+            authId: authId
+          }),
+          call: this.smbService.deleteJoinAuth(authId)
+        })
+    });
+  }
+
+  updateSelection(selection: CdTableSelection) {
+    this.selection = selection;
   }
 }

src/pybind/mgr/dashboard/frontend/src/app/ceph/smb/smb-usersgroups-list/smb-usersgroups-list.component.ts
@@ -14,11 +14,22 @@ import { Permission } from '~/app/shared/models/permissions';
 import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
 import { SmbService } from '~/app/shared/api/smb.service';
 import { SMBUsersGroups } from '../smb.model';
+import { Router } from '@angular/router';
+import { Icons } from '~/app/shared/enum/icons.enum';
+import { CdTableSelection } from '~/app/shared/models/cd-table-selection';
+import { URLBuilderService } from '~/app/shared/services/url-builder.service';
+import { CriticalConfirmationModalComponent } from '~/app/shared/components/critical-confirmation-modal/critical-confirmation-modal.component';
+import { FinishedTask } from '~/app/shared/models/finished-task';
+import { ModalCdsService } from '~/app/shared/services/modal-cds.service';
+import { TaskWrapperService } from '~/app/shared/services/task-wrapper.service';
+
+export const USERSGROUPS_URL = '/cephfs/smb/usersgroups';
 
 @Component({
   selector: 'cd-smb-users-list',
   templateUrl: './smb-usersgroups-list.component.html',
-  styleUrls: ['./smb-usersgroups-list.component.scss']
+  styleUrls: ['./smb-usersgroups-list.component.scss'],
+  providers: [{ provide: URLBuilderService, useValue: new URLBuilderService(USERSGROUPS_URL) }]
 })
 export class SmbUsersgroupsListComponent extends ListWithDetails implements OnInit {
   @ViewChild('groupsNamesTpl', { static: true })
@@ -30,11 +41,16 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
 
   usersGroups$: Observable<SMBUsersGroups[]>;
   subject$ = new BehaviorSubject<SMBUsersGroups[]>([]);
+  selection: CdTableSelection = new CdTableSelection();
 
   constructor(
+    private router: Router,
+    private urlBuilder: URLBuilderService,
     private authStorageService: AuthStorageService,
     public actionLabels: ActionLabelsI18n,
-    private smbService: SmbService
+    private smbService: SmbService,
+    private modalService: ModalCdsService,
+    private taskWrapper: TaskWrapperService
   ) {
     super();
     this.permission = this.authStorageService.getPermissions().smb;
@@ -43,7 +59,7 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
   ngOnInit() {
     this.columns = [
       {
-        name: $localize`ID`,
+        name: $localize`Name`,
         prop: 'users_groups_id',
         flexGrow: 2
       },
@@ -59,9 +75,34 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
         flexGrow: 2
       },
       {
-        name: $localize`Linked to`,
+        name: $localize`Linked to cluster`,
         prop: 'values.linked_to_cluster',
         flexGrow: 2
+      }
+    ];
+
+    this.tableActions = [
+      {
+        name: `${this.actionLabels.CREATE} standalone`,
+        permission: 'create',
+        icon: Icons.add,
+        click: () => this.router.navigate([this.urlBuilder.getCreate()]),
+        canBePrimary: (selection: CdTableSelection) => !selection.hasSelection
+      },
+      {
+        name: this.actionLabels.EDIT,
+        permission: 'update',
+        icon: Icons.edit,
+        click: () =>
+          this.router.navigate([
+            this.urlBuilder.getEdit(String(this.selection.first().users_groups_id))
+          ])
+      },
+      {
+        name: this.actionLabels.DELETE,
+        permission: 'delete',
+        icon: Icons.destroy,
+        click: () => this.openDeleteModal()
       }
     ];
 
@@ -79,5 +120,25 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
 
   loadUsersGroups() {
     this.subject$.next([]);
+  }
+
+  openDeleteModal() {
+    const usersGroupsId = this.selection.first().users_groups_id;
+
+    this.modalService.show(CriticalConfirmationModalComponent, {
+      itemDescription: $localize`Users and groups access resource`,
+      itemNames: [usersGroupsId],
+      submitActionObservable: () =>
+        this.taskWrapper.wrapTaskAroundCall({
+          task: new FinishedTask('smb/usersgroups/remove', {
+            usersGroupsId: usersGroupsId
+          }),
+          call: this.smbService.deleteUsersgroups(usersGroupsId)
+        })
+    });
+  }
+
+  updateSelection(selection: CdTableSelection) {
+    this.selection = selection;
   }
 }

ceph/ceph-ci

src/pybind/mgr/dashboard/frontend/src/app/app-routing.module.ts
@@ -54,6 +54,8 @@ import { MultiClusterDetailsComponent } from './ceph/cluster/multi-cluster/multi
 import { SmbClusterFormComponent } from './ceph/smb/smb-cluster-form/smb-cluster-form.component';
 import { SmbTabsComponent } from './ceph/smb/smb-tabs/smb-tabs.component';
 import { SmbShareFormComponent } from './ceph/smb/smb-share-form/smb-share-form.component';
+import { SmbJoinAuthFormComponent } from './ceph/smb/smb-join-auth-form/smb-join-auth-form.component';
+import { SmbUsersgroupsFormComponent } from './ceph/smb/smb-usersgroups-form/smb-usersgroups-form.component';
 
 @Injectable()
 export class PerformanceCounterBreadcrumbsResolver extends BreadcrumbsResolver {
@@ -457,6 +459,26 @@ const routes: Routes = [
                 path: `share/${URLVerbs.CREATE}/:clusterId`,
                 component: SmbShareFormComponent,
                 data: { breadcrumbs: ActionLabels.CREATE }
+              },
+              {
+                path: `ad/${URLVerbs.CREATE}`,
+                component: SmbJoinAuthFormComponent,
+                data: { breadcrumbs: ActionLabels.CREATE }
+              },
+              {
+                path: `ad/${URLVerbs.EDIT}/:authId`,
+                component: SmbJoinAuthFormComponent,
+                data: { breadcrumbs: ActionLabels.EDIT }
+              },
+              {
+                path: `standalone/${URLVerbs.CREATE}`,
+                component: SmbUsersgroupsFormComponent,
+                data: { breadcrumbs: ActionLabels.CREATE }
+              },
+              {
+                path: `standalone/${URLVerbs.EDIT}/:usersGroupsId`,
+                component: SmbUsersgroupsFormComponent,
+                data: { breadcrumbs: ActionLabels.EDIT }
               }
             ]
           }

src/pybind/mgr/dashboard/frontend/src/app/ceph/smb/smb-join-auth-list/smb-join-auth-list.component.ts
@@ -9,11 +9,22 @@ import { CdTableFetchDataContext } from '~/app/shared/models/cd-table-fetch-data
 import { Permission } from '~/app/shared/models/permissions';
 import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
 import { SMBJoinAuth } from '../smb.model';
+import { Router } from '@angular/router';
+import { Icons } from '~/app/shared/enum/icons.enum';
+import { CdTableSelection } from '~/app/shared/models/cd-table-selection';
+import { URLBuilderService } from '~/app/shared/services/url-builder.service';
+import { DeleteConfirmationModalComponent } from '~/app/shared/components/delete-confirmation-modal/delete-confirmation-modal.component';
+import { ModalCdsService } from '~/app/shared/services/modal-cds.service';
+import { FinishedTask } from '~/app/shared/models/finished-task';
+import { TaskWrapperService } from '~/app/shared/services/task-wrapper.service';
+
+export const JOINAUTH_URL = '/cephfs/smb/ad';
 
 @Component({
   selector: 'cd-smb-join-auth-list',
   templateUrl: './smb-join-auth-list.component.html',
-  styleUrls: ['./smb-join-auth-list.component.scss']
+  styleUrls: ['./smb-join-auth-list.component.scss'],
+  providers: [{ provide: URLBuilderService, useValue: new URLBuilderService(JOINAUTH_URL) }]
 })
 export class SmbJoinAuthListComponent implements OnInit {
   columns: CdTableColumn[];
@@ -23,11 +34,16 @@ export class SmbJoinAuthListComponent implements OnInit {
 
   joinAuth$: Observable<SMBJoinAuth[]>;
   subject$ = new BehaviorSubject<SMBJoinAuth[]>([]);
+  selection: CdTableSelection = new CdTableSelection();
 
   constructor(
+    private router: Router,
+    private urlBuilder: URLBuilderService,
     private authStorageService: AuthStorageService,
     public actionLabels: ActionLabelsI18n,
-    private smbService: SmbService
+    private smbService: SmbService,
+    private modalService: ModalCdsService,
+    private taskWrapper: TaskWrapperService
   ) {
     this.permission = this.authStorageService.getPermissions().smb;
   }
@@ -35,7 +51,7 @@ export class SmbJoinAuthListComponent implements OnInit {
   ngOnInit() {
     this.columns = [
       {
-        name: $localize`ID`,
+        name: $localize`Name`,
         prop: 'auth_id',
         flexGrow: 2
       },
@@ -45,9 +61,32 @@ export class SmbJoinAuthListComponent implements OnInit {
         flexGrow: 2
       },
       {
-        name: $localize`Linked to Cluster`,
+        name: $localize`Linked to cluster`,
         prop: 'linked_to_cluster',
         flexGrow: 2
+      }
+    ];
+
+    this.tableActions = [
+      {
+        name: `${this.actionLabels.CREATE} AD`,
+        permission: 'create',
+        icon: Icons.add,
+        click: () => this.router.navigate([this.urlBuilder.getCreate()]),
+        canBePrimary: (selection: CdTableSelection) => !selection.hasSelection
+      },
+      {
+        name: this.actionLabels.EDIT,
+        permission: 'update',
+        icon: Icons.edit,
+        click: () =>
+          this.router.navigate([this.urlBuilder.getEdit(String(this.selection.first().auth_id))])
+      },
+      {
+        name: this.actionLabels.DELETE,
+        permission: 'update',
+        icon: Icons.destroy,
+        click: () => this.openDeleteModal()
       }
     ];
 
@@ -65,5 +104,25 @@ export class SmbJoinAuthListComponent implements OnInit {
 
   loadJoinAuth() {
     this.subject$.next([]);
+  }
+
+  openDeleteModal() {
+    const authId = this.selection.first().auth_id;
+
+    this.modalService.show(DeleteConfirmationModalComponent, {
+      itemDescription: $localize`Active directory access resource`,
+      itemNames: [authId],
+      submitActionObservable: () =>
+        this.taskWrapper.wrapTaskAroundCall({
+          task: new FinishedTask('smb/ad/remove', {
+            authId: authId
+          }),
+          call: this.smbService.deleteJoinAuth(authId)
+        })
+    });
+  }
+
+  updateSelection(selection: CdTableSelection) {
+    this.selection = selection;
   }
 }

src/pybind/mgr/dashboard/frontend/src/app/ceph/smb/smb-usersgroups-list/smb-usersgroups-list.component.ts
@@ -14,11 +14,22 @@ import { Permission } from '~/app/shared/models/permissions';
 import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
 import { SmbService } from '~/app/shared/api/smb.service';
 import { SMBUsersGroups } from '../smb.model';
+import { Router } from '@angular/router';
+import { Icons } from '~/app/shared/enum/icons.enum';
+import { CdTableSelection } from '~/app/shared/models/cd-table-selection';
+import { URLBuilderService } from '~/app/shared/services/url-builder.service';
+import { DeleteConfirmationModalComponent } from '~/app/shared/components/delete-confirmation-modal/delete-confirmation-modal.component';
+import { FinishedTask } from '~/app/shared/models/finished-task';
+import { ModalCdsService } from '~/app/shared/services/modal-cds.service';
+import { TaskWrapperService } from '~/app/shared/services/task-wrapper.service';
+
+export const USERSGROUPS_URL = '/cephfs/smb/standalone';
 
 @Component({
   selector: 'cd-smb-users-list',
   templateUrl: './smb-usersgroups-list.component.html',
-  styleUrls: ['./smb-usersgroups-list.component.scss']
+  styleUrls: ['./smb-usersgroups-list.component.scss'],
+  providers: [{ provide: URLBuilderService, useValue: new URLBuilderService(USERSGROUPS_URL) }]
 })
 export class SmbUsersgroupsListComponent extends ListWithDetails implements OnInit {
   @ViewChild('groupsNamesTpl', { static: true })
@@ -30,11 +41,16 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
 
   usersGroups$: Observable<SMBUsersGroups[]>;
   subject$ = new BehaviorSubject<SMBUsersGroups[]>([]);
+  selection: CdTableSelection = new CdTableSelection();
 
   constructor(
+    private router: Router,
+    private urlBuilder: URLBuilderService,
     private authStorageService: AuthStorageService,
     public actionLabels: ActionLabelsI18n,
-    private smbService: SmbService
+    private smbService: SmbService,
+    private modalService: ModalCdsService,
+    private taskWrapper: TaskWrapperService
   ) {
     super();
     this.permission = this.authStorageService.getPermissions().smb;
@@ -43,7 +59,7 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
   ngOnInit() {
     this.columns = [
       {
-        name: $localize`ID`,
+        name: $localize`Name`,
         prop: 'users_groups_id',
         flexGrow: 2
       },
@@ -59,9 +75,34 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
         flexGrow: 2
       },
       {
-        name: $localize`Linked to`,
+        name: $localize`Linked to cluster`,
         prop: 'values.linked_to_cluster',
         flexGrow: 2
+      }
+    ];
+
+    this.tableActions = [
+      {
+        name: `${this.actionLabels.CREATE} standalone`,
+        permission: 'create',
+        icon: Icons.add,
+        click: () => this.router.navigate([this.urlBuilder.getCreate()]),
+        canBePrimary: (selection: CdTableSelection) => !selection.hasSelection
+      },
+      {
+        name: this.actionLabels.EDIT,
+        permission: 'update',
+        icon: Icons.edit,
+        click: () =>
+          this.router.navigate([
+            this.urlBuilder.getEdit(String(this.selection.first().users_groups_id))
+          ])
+      },
+      {
+        name: this.actionLabels.DELETE,
+        permission: 'delete',
+        icon: Icons.destroy,
+        click: () => this.openDeleteModal()
       }
     ];
 
@@ -79,5 +120,25 @@ export class SmbUsersgroupsListComponent extends ListWithDetails implements OnIn
 
   loadUsersGroups() {
     this.subject$.next([]);
+  }
+
+  openDeleteModal() {
+    const usersGroupsId = this.selection.first().users_groups_id;
+
+    this.modalService.show(DeleteConfirmationModalComponent, {
+      itemDescription: $localize`Users and groups access resource`,
+      itemNames: [usersGroupsId],
+      submitActionObservable: () =>
+        this.taskWrapper.wrapTaskAroundCall({
+          task: new FinishedTask('smb/standalone/remove', {
+            usersGroupsId: usersGroupsId
+          }),
+          call: this.smbService.deleteUsersgroups(usersGroupsId)
+        })
+    });
+  }
+
+  updateSelection(selection: CdTableSelection) {
+    this.selection = selection;
   }
 }