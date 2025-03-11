e8f507c039c35f04baf867c0c158e5dd55542ed6
Patrick Donnelly <pdonnell@ibm.com>
2025-03-11 10:26:23 -0400

    client: print readable encrypted names
    Replace non-printable characters with '.'.
    
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -1295,7 +1295,7 @@ Dentry *Client::insert_dentry_inode(Dir *dir, const string& dname, LeaseStat *dl
 
 bool Client::_wrap_name(Inode& diri, std::string& dname, std::string& alternate_name)
 {
-  ldout(cct, 20) << __func__ << ": (" << dname << " len=" << dname.size() << ", " << alternate_name << ") on " << diri << dendl;
+  ldout(cct, 20) << __func__ << ": (" << binstrprint(dname) << " len=" << dname.size() << ", " << binstrprint(alternate_name) << ") on " << diri << dendl;
   ceph_assert(dname.size() > 0);
   alternate_name = "";
 
@@ -1317,7 +1317,7 @@ bool Client::_wrap_name(Inode& diri, std::string& dname, std::string& alternate_
         /* confirm valid utf-8 name */
         encoded = boost::locale::conv::to_utf<char>(dname, "UTF-8", boost::locale::conv::stop);
       } catch (const boost::locale::conv::conversion_error& e) {
-        ldout(cct, 2) << "`" << dname << "' is not valid utf-8: " << e.what() << dendl;
+        ldout(cct, 2) << "`" << binstrprint(dname) << "' is not valid utf-8: " << e.what() << dendl;
         return false;
       }
     } else if (!encoding.empty()) {
@@ -1356,7 +1356,7 @@ bool Client::_wrap_name(Inode& diri, std::string& dname, std::string& alternate_
     std::string folded;
     if (is_insensitive) {
       if (normalized.empty()) {
-        ldout(cct, 2) << __func__ << " normalization is required before case folding: " << dname << dendl;
+        ldout(cct, 2) << __func__ << " normalization is required before case folding: " << binstrprint(dname) << dendl;
         return false;
       }
       try {
@@ -1406,7 +1406,7 @@ bool Client::_wrap_name(Inode& diri, std::string& dname, std::string& alternate_
 
 std::string Client::_unwrap_name(Inode& diri, const std::string& dname, const std::string& alternate_name)
 {
-  ldout(cct, 20) << __func__ << ": (" << dname << ", " << alternate_name << ") on " << diri << dendl;
+  ldout(cct, 20) << __func__ << ": (" << binstrprint(dname) << ", " << binstrprint(alternate_name) << ") on " << diri << dendl;
   std::string newdname = dname;
   std::string newaltn = alternate_name;
 
@@ -1441,7 +1441,7 @@ std::string Client::_unwrap_name(Inode& diri, const std::string& dname, const st
     /* no reverse of normalization / encoding */
 
     if (is_insensitive) {
-      ldout(cct, 25) << __func__ << ":  = " << alternate_name << dendl;
+      ldout(cct, 25) << __func__ << ":  = " << binstrprint(alternate_name) << dendl;
       newdname = newaltn;
     }
   }
@@ -7789,7 +7789,7 @@ int Client::path_walk(InodeRef dirinode, const filepath& origpath, walk_dentry_r
   int symlinks = 0;
   unsigned i = 0;
 
-  ldout(cct, 10) << __func__ << ": cur=" << *diri << " path=" << path << dendl;
+  ldout(cct, 10) << __func__ << ": cur=" << *diri << " path=" << binstrprint(path.get_path()) << dendl;
 
   if (path.depth() == 0) {
     /* diri/dname can also be used as a filepath; or target */
@@ -7802,8 +7802,8 @@ int Client::path_walk(InodeRef dirinode, const filepath& origpath, walk_dentry_r
   while (i < path.depth() && diri) {
     int caps = 0;
     dname = path[i];
-    ldout(cct, 10) << " " << i << " " << *diri << " " << dname << dendl;
-    ldout(cct, 20) << "  (path is " << path << ")" << dendl;
+    ldout(cct, 10) << " " << binstrprint(dname) << " in " << *diri << dendl;
+    ldout(cct, 20) << "  (i is " << i << " path is " << binstrprint(path.get_path()) << ")" << dendl;
     InodeRef next;
     if (should_check_perms()) {
       int r = may_lookup(diri.get(), perms);
@@ -7867,6 +7867,7 @@ int Client::path_walk(InodeRef dirinode, const filepath& origpath, walk_dentry_r
           ret = -EPERM;
           goto out;
         }
+        ldout(cct, 25) << "decrypted symlink is: " << binstrprint(symlink) << dendl;
       } else {
         symlink = next->symlink;
       }
@@ -15851,6 +15852,7 @@ int Client::_symlink(Inode *dir, const char *name, const char *target,
       delete req;
       return r;
     }
+    ldout(cct, 25) << "encrypted symlink is: " << binstrprint(enc_target) << dendl;
     req->set_string2(enc_target.c_str());
   } else {
     req->set_string2(target);
