1dace0fa6701a8c102a452b0f4af1a2805d9aed1
Bill Scales <bill_scales@uk.ibm.com>
2025-04-09 17:21:00 +0100

    EDITS: osd: EC Optimizations: Get missing changes for partial logs
    To be squashed after review
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

--- src/osd/PeeringState.cc
+++ src/osd/PeeringState.cc
@@ -7544,12 +7544,13 @@ PeeringState::GetMissing::GetMissing(my_context ctx)
       continue;
     }
 
-    // Partial writes - if the peer log is only divergent because of partial writes then
+    // If the peer log is only divergent because of partial writes then
     // roll forward the peer to cover writes it was not involved in.
     if (pi.last_update < ps->info.last_update) {
-      psdout(0) << "BILL_GET_MISSING: osd." << *i << " has version " << pi.last_update << " which is behind authorative log " << ps->info.last_update << dendl;
-      // Search backwards through log looking for a match with peer's head entry
-      mempool::osd_pglog::list<pg_log_entry_t>::const_iterator p = ps->pg_log.get_log().log.end();
+      // Search backwards through log looking for a match with peer's head
+      // entry
+      mempool::osd_pglog::list<pg_log_entry_t>::const_iterator p =
+	ps->pg_log.get_log().log.end();
       while (p != ps->pg_log.get_log().log.begin()) {
 	--p;
 	if (p->version.version <= pi.last_update.version) {
@@ -7558,40 +7559,50 @@ PeeringState::GetMissing::GetMissing(my_context ctx)
       }
       if (pi.last_update == pi.last_complete &&
 	  p->version == pi.last_update) {
-	// Matched peer's head entry - see if we can advance last_update because of partial written shards
+	// Matched peer's head entry - see if we can advance last_update
+	// because of partial written shards
 	eversion_t old_last_update = pi.last_update;
 	if (ps->info.partial_writes_last_complete.contains(i->shard) &&
-	    ps->info.partial_writes_last_complete[i->shard].first < old_last_update) {
-	  old_last_update = ps->info.partial_writes_last_complete[i->shard].first;
+	    ps->info.partial_writes_last_complete[i->shard].first <
+	    old_last_update) {
+	  old_last_update =
+	    ps->info.partial_writes_last_complete[i->shard].first;
 	}
 	++p;
+	bool advanced = false;
 	while (p != ps->pg_log.get_log().log.end()) {
-	  psdout(0) << "BILL_GET_MISSING: log entry " << p->version << " has written_shards=" << p->written_shards << dendl;
-	  if (!p->is_written_shard(i->shard)) {
-	    psdout(0) << "BILL_GET_MISSING: log entry advancing last_update because of partial write" << dendl;
-	    pi.last_update = p->version;
-	    pi.last_complete = p->version;
-	    // Update partial_writes_last_complete, this can be used by proc_replica_info to avoid repeating this work
-	    // and will explain to PeeringState::Stray::react(const MInfoRec& infoevt) why last_update and complete
-	    // have been advanced
-	    if (ps->info.partial_writes_last_complete.contains(i->shard)) {
-	      // Existing pwlc entry - only update if p->version is newer
-	      if (ps->info.partial_writes_last_complete[i->shard].second < p->version) {
-		ps->info.partial_writes_last_complete[i->shard] = std::pair(old_last_update, p->version);
-	      }
-	    } else {
-	      // No existing pwlc entry - create one
-	      ps->info.partial_writes_last_complete[i->shard] = std::pair(old_last_update, p->version);
-	    }
-	    psdout(0) << "BILL_GET_MISSING2: " << i->shard << " pwlc=" << ps->info.partial_writes_last_complete.at(i->shard) << dendl;
-	  } else {
-	    psdout(0) << "BILL_GET_MISSING: log entry is divergent - rewinding one entry" << dendl;
+	  if (p->is_written_shard(i->shard)) {
+	    psdout(20) << "log entry " << p->version
+		       << " written_shards=" << p->written_shards
+		       << " is divergent" << dendl;
 	    break;
 	  }
+	  pi.last_update = p->version;
+	  pi.last_complete = p->version;
+	  // Update partial_writes_last_complete
+	  if (ps->info.partial_writes_last_complete.contains(i->shard)) {
+	    // Existing pwlc entry - only update if p->version is newer
+	    if (ps->info.partial_writes_last_complete[i->shard].second <
+		p->version) {
+	      ps->info.partial_writes_last_complete[i->shard] =
+		std::pair(old_last_update, p->version);
+	    }
+	  } else {
+	    // No existing pwlc entry - create one
+	    ps->info.partial_writes_last_complete[i->shard] =
+	      std::pair(old_last_update, p->version);
+	  }
+	  advanced = true;
 	  ++p;
 	}
+	if (advanced) {
+	  psdout(20) << "shard " << i->shard << " pwlc="
+		     << ps->info.partial_writes_last_complete.at(i->shard)
+		     << " last_complete=" << ps->info.last_complete
+		     << " last_update=" << pi.last_update
+		     << dendl;
+	}
       }
-      psdout(0) << "BILL_GET_MISSING: " << ps->info.last_complete << " " << pi.last_update << dendl;
     }
 
     if (pi.last_update == pi.last_complete &&  // peer has no missing
