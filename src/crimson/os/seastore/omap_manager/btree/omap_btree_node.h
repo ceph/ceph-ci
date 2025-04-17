2084cf7ad762d1b68cc2c70374f6343f4e5b7a2a
chunmei liu <chunmei.liu@ibm.com>
2025-04-17 01:03:34 -0700

    crimson/os: optimize omap_rm_key_range
    not use omap_list and remove key by key, just iterate to remove key
    range.
    
    Signed-off-by: chunmei liu <chunmei.liu@ibm.com>

--- src/crimson/os/seastore/omap_manager/btree/omap_btree_node.h
+++ src/crimson/os/seastore/omap_manager/btree/omap_btree_node.h
@@ -30,7 +30,7 @@ enum class mutation_status_t : uint8_t {
   WAS_SPLIT = 1,
   NEED_MERGE = 2,
   FAIL = 3
-};
+ };
 
 struct OMapNode : LogicalChildNode {
   using base_iertr = OMapManager::base_iertr;
@@ -72,6 +72,13 @@ struct OMapNode : LogicalChildNode {
     omap_context_t oc,
     const std::string &key,
     const ceph::bufferlist &value) = 0;
+
+  using rm_key_range_iertr = base_iertr;
+  using rm_key_range_ret = rm_key_range_iertr::future<mutation_result_t>;
+  virtual rm_key_range_ret rm_key_range(
+    omap_context_t oc,
+    const std::string &first,
+    const std::string &last) = 0;
 
   using rm_key_iertr = base_iertr;
   using rm_key_ret = rm_key_iertr::future<mutation_result_t>;
