ceph/ceph

mgr/cepahdm: adding filtering options for 'orch certmgr key ls' cmd
Signed-off-by: Redouane Kachach <rkachach@ibm.com>

src/pybind/mgr/cephadm/cert_mgr.py
@@ -300,10 +300,12 @@ class CertMgr:
 
         return ls
 
-    def key_ls(self) -> Dict:
+    def key_ls(self, show_cephadm_signed: bool = False) -> Dict:
         key_objects: List = self.key_store.list_tlsobjects()
         ls: Dict = {}
         for key_name, key_obj, target in key_objects:
+            if not show_cephadm_signed and self.is_cephadm_signed_entity(key_name):
+                continue
             priv_key_info = get_private_key_info(key_obj.key)
             key_scope = self.get_key_scope(key_name)
             if key_name not in ls:

src/pybind/mgr/cephadm/module.py
@@ -3281,8 +3281,8 @@ Then run the following:
         return report
 
     @handle_orch_error
-    def cert_store_key_ls(self) -> Dict[str, Any]:
-        return self.cert_mgr.key_ls()
+    def cert_store_key_ls(self, show_cephadm_signed: bool = False) -> Dict[str, Any]:
+        return self.cert_mgr.key_ls(show_cephadm_signed)
 
     @handle_orch_error
     def cert_store_get_cert(

src/pybind/mgr/orchestrator/_interface.py
@@ -575,7 +575,7 @@ class Orchestrator(object):
     def cert_store_cert_check(self) -> OrchResult[List[str]]:
         raise NotImplementedError()
 
-    def cert_store_key_ls(self) -> OrchResult[Dict[str, Any]]:
+    def cert_store_key_ls(self, show_cephadm_signed: bool = False) -> OrchResult[Dict[str, Any]]:
         raise NotImplementedError()
 
     def cert_store_get_cert(

src/pybind/mgr/orchestrator/module.py
@@ -1197,8 +1197,10 @@ class OrchestratorCli(OrchestratorClientMixin, MgrModule,
             return HandleCommandResult(stdout=result_str)
 
     @_cli_read_command('orch certmgr key ls')
-    def _cert_store_key_ls(self, format: Format = Format.plain) -> HandleCommandResult:
-        completion = self.cert_store_key_ls()
+    def _cert_store_key_ls(self,
+                           show_cephadm_signed: bool = False,
+                           format: Format = Format.plain) -> HandleCommandResult:
+        completion = self.cert_store_key_ls(show_cephadm_signed)
         key_ls = raise_if_exception(completion)
         if format != Format.plain:
             return HandleCommandResult(stdout=to_format(key_ls, format, many=False, cls=None))

ceph/ceph-ci

src/pybind/mgr/cephadm/cert_mgr.py
@@ -183,8 +183,6 @@ class CertMgr:
 
         :param service_name: The name of the service.
         """
-        ss_cert_name = self.self_signed_cert(service_name)
-        ss_key_name = self.self_signed_key(service_name)
         self.cert_store.add_entity(self.self_signed_cert(service_name), TLSObjectScope.HOST)
         self.key_store.add_entity(self.self_signed_key(service_name), TLSObjectScope.HOST)
 
@@ -354,10 +352,12 @@ class CertMgr:
 
         return ls
 
-    def key_ls(self) -> Dict:
+    def key_ls(self, include_cephadm_signed: bool = False) -> Dict:
         key_objects: List = self.key_store.list_tlsobjects()
         ls: Dict = {}
         for key_name, key_obj, target in key_objects:
+            if not include_cephadm_signed and self.is_cephadm_signed_entity(key_name):
+                continue
             priv_key_info = get_private_key_info(key_obj.key)
             key_scope = self.get_key_scope(key_name)
             if key_name not in ls:

src/pybind/mgr/cephadm/module.py
@@ -3291,8 +3291,8 @@ Then run the following:
         return report
 
     @handle_orch_error
-    def cert_store_key_ls(self) -> Dict[str, Any]:
-        return self.cert_mgr.key_ls()
+    def cert_store_key_ls(self, include_cephadm_signed: bool = False) -> Dict[str, Any]:
+        return self.cert_mgr.key_ls(include_cephadm_signed)
 
     @handle_orch_error
     def cert_store_get_cert(

src/pybind/mgr/orchestrator/_interface.py
@@ -575,7 +575,7 @@ class Orchestrator(object):
     def cert_store_cert_check(self) -> OrchResult[List[str]]:
         raise NotImplementedError()
 
-    def cert_store_key_ls(self) -> OrchResult[Dict[str, Any]]:
+    def cert_store_key_ls(self, include_cephadm_signed: bool = False) -> OrchResult[Dict[str, Any]]:
         raise NotImplementedError()
 
     def cert_store_get_cert(

src/pybind/mgr/orchestrator/module.py
@@ -1197,8 +1197,10 @@ class OrchestratorCli(OrchestratorClientMixin, MgrModule,
             return HandleCommandResult(stdout=result_str)
 
     @_cli_read_command('orch certmgr key ls')
-    def _cert_store_key_ls(self, format: Format = Format.plain) -> HandleCommandResult:
-        completion = self.cert_store_key_ls()
+    def _cert_store_key_ls(self,
+                           include_cephadm_signed: bool = False,
+                           format: Format = Format.plain) -> HandleCommandResult:
+        completion = self.cert_store_key_ls(include_cephadm_signed)
         key_ls = raise_if_exception(completion)
         if format != Format.plain:
             return HandleCommandResult(stdout=to_format(key_ls, format, many=False, cls=None))