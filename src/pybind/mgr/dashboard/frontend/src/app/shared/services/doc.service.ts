749a178e455beb4485437f69eb1dfa106411f1c7
Nizamudeen A <nia@redhat.com>
2025-05-15 12:09:41 +0530

    mgr/dashboard: updating urls for 9.0
    Resolves: rhbz#2366187
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 1c3fcf9c3b9ee41f70eca200a64710ead2b026ec)
    (cherry picked from commit 7904604048d51ba16a3c0d466112eb77d917b7b1)
    
    mgr/dashboard: updating urls for 9.0
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 1f8fd41b185bdd1aa92fe13208ce499e735c94ca)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/shared/services/doc.service.ts
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/shared/services/doc.service.ts

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/doc.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/doc.service.ts
@@ -23,32 +23,60 @@ export class DocService {
     });
   }
 
-  urlGenerator(section: string, release = 'main'): string {
-    const docVersion = release === 'main' ? 'latest' : release;
-    const domain = `https://docs.ceph.com/en/${docVersion}/`;
-    const domainCeph = `https://ceph.io`;
+  urlGenerator(section: string, release = 'tentacle'): string {
+    const docVersion = release === 'tentacle' ? '9' : release;
+    let sections = {};
 
-    const sections = {
-      iscsi: `${domain}mgr/dashboard/#enabling-iscsi-management`,
-      prometheus: `${domain}mgr/dashboard/#enabling-prometheus-alerting`,
-      'nfs-ganesha': `${domain}mgr/dashboard/#configuring-nfs-ganesha-in-the-dashboard`,
-      'rgw-nfs': `${domain}radosgw/nfs`,
-      rgw: `${domain}mgr/dashboard/#enabling-the-object-gateway-management-frontend`,
-      'rgw-multisite': `${domain}/radosgw/multisite/#failover-and-disaster-recovery`,
-      multisite: `${domain}/radosgw/multisite`,
-      dashboard: `${domain}mgr/dashboard`,
-      grafana: `${domain}mgr/dashboard/#enabling-the-embedding-of-grafana-dashboards`,
-      orch: `${domain}mgr/orchestrator`,
-      pgs: `${domain}/rados/operations/placement-groups/#choosing-number-of-placement-groups`,
-      help: `${domainCeph}/en/users/`,
-      security: `${domainCeph}/en/security/`,
-      trademarks: `${domainCeph}/en/trademarks/`,
-      'dashboard-landing-page-status': `${domain}mgr/dashboard/#dashboard-landing-page-status`,
-      'dashboard-landing-page-performance': `${domain}mgr/dashboard/#dashboard-landing-page-performance`,
-      'dashboard-landing-page-capacity': `${domain}mgr/dashboard/#dashboard-landing-page-capacity`,
-      'dashboard-side-panel': `${domain}/rados/operations/health-checks/`
-    };
+    if (environment.build === 'ibm') {
+      // overriding the ibm docs to serve from 9.0.0 link. not overriding the
+      // global variable since rh still uses old versioning format
+      const domain = `https://www.ibm.com/docs/storage-ceph/${docVersion}.0.0?topic=`;
+      const domainIBM = `https://www.ibm.com/support/customer/csol/terms/`;
 
+      sections = {
+        iscsi: `${domain}dashboard-management-iscsi-functions-using-ceph`,
+        prometheus: `${domain}dashboard-managing-alerts`,
+        'nfs-ganesha': `${domain}dashboard-managing-nfs-exports`,
+        'rgw-nfs': `${domain}dashboard-managing-nfs-exports`,
+        rgw: `${domain}dashboard-managing-ceph-object-gateway`,
+        'rgw-multisite': `${domain}zone-failover-disaster-recovery`,
+        multisite: `${domain}gateway-multi-site-configuration-administration`,
+        dashboard: `${domain}dashboard`,
+        grafana: `${domain}alerts-enabling-monitoring-stack`,
+        orch: `${domain}operations-introduction-ceph-orchestrator`,
+        pgs: `https://access.redhat.com/labs/cephpgc/`,
+        help: domain,
+        terms: `${domainIBM}?id=L-KDIY-CJHJCJ&lc=en#detail-document`,
+        privacy: `${domainIBM}?id=Z126-7870&lc=en#detail-document`,
+        'dashboard-landing-page-status': `${domain}dashboard-understanding-landing-page-ceph`,
+        'dashboard-landing-page-performance': `${domain}dashboard-understanding-landing-page-ceph`,
+        'dashboard-landing-page-capacity': `${domain}dashboard-understanding-landing-page-ceph`,
+        'dashboard-side-panel': `${domain}/rados/operations/health-checks/`
+      };
+    } else {
+      const domain = `https://docs.redhat.com/en/documentation/red_hat_ceph_storage/${docVersion}/html/`;
+      const domainRedHat = `https://www.redhat.com/en/about/`;
+
+      sections = {
+        iscsi: `${domain}dashboard_guide/management-of-block-devices-using-the-ceph-dashboard#management-of-iscsi-functions-on-the-ceph-dashboard`,
+        prometheus: `${domain}dashboard_guide/management-of-alerts-on-the-ceph-dashboard`,
+        'nfs-ganesha': `${domain}dashboard_guide/management-of-nfs-ganesha-exports-on-the-ceph-dashboard#configuring-nfs-ganesha-daemons-on-the-ceph-dashboard_dash`,
+        'rgw-nfs': `${domain}dashboard_guide/management-of-nfs-ganesha-exports-on-the-ceph-dashboard`,
+        rgw: `${domain}dashboard_guide/management-of-ceph-object-gateway-using-the-dashboard`,
+        'rgw-multisite': `${domain}object_gateway_guide/multisite-configuration-and-administration#failover-and-disaster-recovery-rgw`,
+        multisite: `${domain}object_gateway_guide/multisite-configuration-and-administration`,
+        dashboard: `${domain}dashboard_guide/`,
+        grafana: `${domain}dashboard_guide/management-of-alerts-on-the-ceph-dashboard#enabling-monitoring-stack_dash`,
+        orch: `${domain}operations_guide/introduction-to-the-ceph-orchestrator`,
+        pgs: `https://access.redhat.com/labs/cephpgc/`,
+        help: `https://docs.redhat.com/en/documentation/red_hat_ceph_storage/`,
+        terms: `${domainRedHat}all-policies-guidelines/`,
+        privacy: `${domainRedHat}privacy-policy/`,
+        'dashboard-landing-page-status': `${domain}dashboard_guide/index#understanding-the-landing-page_dash`,
+        'dashboard-landing-page-performance': `${domain}dashboard_guide/index#understanding-the-landing-page_dash`,
+        'dashboard-landing-page-capacity': `${domain}dashboard_guide/index#understanding-the-landing-page_dash`
+      };
+    }
     return sections[section];
   }
 
