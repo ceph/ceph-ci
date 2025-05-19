xxhdx1985126/ceph

crimson/os/seastore: refactor LBAMapping and BackrefMapping
Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

src/crimson/os/seastore/lba_mapping.cc
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
-#include "lba_mapping.h"
+#include "crimson/os/seastore/lba_mapping.h"
 
 namespace crimson::os::seastore {
 
@@ -20,25 +20,55 @@ std::ostream &operator<<(std::ostream &out, const LBAMapping &rhs)
   return out;
 }
 
-std::ostream &operator<<(std::ostream &out, const lba_pin_list_t &rhs)
+std::ostream &operator<<(std::ostream &out, const lba_mapping_list_t &rhs)
 {
   bool first = true;
   out << '[';
   for (const auto &i: rhs) {
-    out << (first ? "" : ",") << *i;
+    out << (first ? "" : ",") << i;
     first = false;
   }
   return out << ']';
 }
 
-LBAMappingRef LBAMapping::duplicate() const {
-  auto ret = _duplicate(ctx);
-  ret->range = range;
-  ret->value = value;
-  ret->parent = parent;
-  ret->len = len;
-  ret->pos = pos;
-  return ret;
+using lba_manager::btree::LBALeafNode;
+
+get_child_ret_t<LBALeafNode, LogicalChildNode>
+LBAMapping::get_logical_extent(Transaction &t)
+{
+  assert(is_linked_direct());
+  ceph_assert(direct_cursor->is_valid());
+  ceph_assert(direct_cursor->ctx.trans.get_trans_id()
+	      == t.get_trans_id());
+  assert(!direct_cursor->is_end());
+  auto &i = *direct_cursor;
+  assert(i.pos != BTREENODE_POS_NULL);
+  ceph_assert(t.get_trans_id() == i.ctx.trans.get_trans_id());
+  auto p = direct_cursor->parent->cast<LBALeafNode>();
+  return p->template get_child<LogicalChildNode>(
+    t, i.ctx.cache, i.pos, i.key);
+}
+
+bool LBAMapping::is_stable() const {
+  assert(is_linked_direct());
+  ceph_assert(direct_cursor->is_valid());
+  assert(!direct_cursor->is_end());
+  auto leaf = direct_cursor->parent->cast<LBALeafNode>();
+  return leaf->is_child_stable(
+    direct_cursor->ctx,
+    direct_cursor->pos,
+    direct_cursor->key);
+}
+
+bool LBAMapping::is_data_stable() const {
+  assert(is_linked_direct());
+  ceph_assert(direct_cursor->is_valid());
+  assert(!direct_cursor->is_end());
+  auto leaf = direct_cursor->parent->cast<LBALeafNode>();
+  return leaf->is_child_data_stable(
+    direct_cursor->ctx,
+    direct_cursor->pos,
+    direct_cursor->key);
 }
 
 } // namespace crimson::os::seastore

ceph/ceph-ci

src/crimson/os/seastore/lba_mapping.cc
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
-#include "lba_mapping.h"
+#include "crimson/os/seastore/lba_mapping.h"
 
 namespace crimson::os::seastore {
 
@@ -20,25 +20,55 @@ std::ostream &operator<<(std::ostream &out, const LBAMapping &rhs)
   return out;
 }
 
-std::ostream &operator<<(std::ostream &out, const lba_pin_list_t &rhs)
+std::ostream &operator<<(std::ostream &out, const lba_mapping_list_t &rhs)
 {
   bool first = true;
   out << '[';
   for (const auto &i: rhs) {
-    out << (first ? "" : ",") << *i;
+    out << (first ? "" : ",") << i;
     first = false;
   }
   return out << ']';
 }
 
-LBAMappingRef LBAMapping::duplicate() const {
-  auto ret = _duplicate(ctx);
-  ret->range = range;
-  ret->value = value;
-  ret->parent = parent;
-  ret->len = len;
-  ret->pos = pos;
-  return ret;
+using lba_manager::btree::LBALeafNode;
+
+get_child_ret_t<LBALeafNode, LogicalChildNode>
+LBAMapping::get_logical_extent(Transaction &t)
+{
+  assert(is_linked_direct());
+  ceph_assert(direct_cursor->is_viewable());
+  ceph_assert(direct_cursor->ctx.trans.get_trans_id()
+	      == t.get_trans_id());
+  assert(!direct_cursor->is_end());
+  auto &i = *direct_cursor;
+  assert(i.pos != BTREENODE_POS_NULL);
+  ceph_assert(t.get_trans_id() == i.ctx.trans.get_trans_id());
+  auto p = direct_cursor->parent->cast<LBALeafNode>();
+  return p->template get_child<LogicalChildNode>(
+    t, i.ctx.cache, i.pos, i.key);
+}
+
+bool LBAMapping::is_stable() const {
+  assert(is_linked_direct());
+  ceph_assert(direct_cursor->is_viewable());
+  assert(!direct_cursor->is_end());
+  auto leaf = direct_cursor->parent->cast<LBALeafNode>();
+  return leaf->is_child_stable(
+    direct_cursor->ctx,
+    direct_cursor->pos,
+    direct_cursor->key);
+}
+
+bool LBAMapping::is_data_stable() const {
+  assert(is_linked_direct());
+  ceph_assert(direct_cursor->is_viewable());
+  assert(!direct_cursor->is_end());
+  auto leaf = direct_cursor->parent->cast<LBALeafNode>();
+  return leaf->is_child_data_stable(
+    direct_cursor->ctx,
+    direct_cursor->pos,
+    direct_cursor->key);
 }
 
 } // namespace crimson::os::seastore