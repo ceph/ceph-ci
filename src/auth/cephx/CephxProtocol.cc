9a929fd189c8bf11290ba5d9c6831ce2b19790b8
Patrick Donnelly <pdonnell@ibm.com>
2025-05-21 17:51:10 -0400

    f

--- src/auth/cephx/CephxProtocol.cc
+++ src/auth/cephx/CephxProtocol.cc
@@ -67,8 +67,9 @@ void CephXSessionAuthInfo::print(std::ostream& os) const
   os << "session_auth_info("
      << ceph_entity_type_name(service_id)
      << " id=" << secret_id
+     << " session_key=" << session_key
+     << " service_secret=" << service_secret
      << " ticket.name=" << ticket.name
-     << " ticket.service_secret=" << ticket.service_secret
      << " ticket.global_id=" << ticket.global_id
      << ")";
 }
@@ -88,7 +89,7 @@ bool cephx_build_service_ticket_blob(CephContext *cct, const CephXSessionAuthInf
   ldout(cct, 10) << "build_service_ticket service " << info << dendl;
   blob.secret_id = info.secret_id;
   std::string error;
-  if (!info.service_secret.get_secret().length())
+  if (info.service_secret.empty())
     error = "invalid key";  // Bad key?
   else
     encode_encrypt_enc_bl(cct, ticket_info, info.service_secret, blob.blob, error);
@@ -513,6 +514,7 @@ bool cephx_verify_authorizer(CephContext *cct, const KeyStore& keys,
   }
 
   ldout(cct, 10) << __func__ << ": global_id=" << global_id << dendl;
+  ldout(cct, 30) << __func__ << ": session key=" << ticket_info.session_key << dendl;
 
   // CephXAuthorize
   CephXAuthorize auth_msg;
