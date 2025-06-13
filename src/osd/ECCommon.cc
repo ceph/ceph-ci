d1594bd94bbb0e7a217ca7fc7f997be16cab6ab8
Alex Ainscow <aainscow@uk.ibm.com>
2025-06-13 14:49:39 +0100

    osd: Fix attribute recover in rare recovery scenario
    When recovering attributes, we read them from the first potential primary, then
    if that read failures, attempt to read from another potential primary.
    
    The problem is that the code which calculates which shards to read for a recovery
    only takes into account *data* and not where the attributes are.  As such, if the
    second read only required a non-primary, then the attribute read fails and the
    OSD panics.
    
    The fix is to detect this scenario and perform an empty read to that shard, which
    the attribute-read code can use for attribute reads.
    
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/osd/ECCommon.cc
+++ src/osd/ECCommon.cc
@@ -312,8 +312,8 @@ int ECCommon::ReadPipeline::get_remaining_shards(
     const hobject_t &hoid,
     read_result_t &read_result,
     read_request_t &read_request,
-    const bool for_recovery) {
-  shard_id_map<pg_shard_t> shards(sinfo.get_k_plus_m());
+    const bool for_recovery,
+    bool want_attrs) {
   set<pg_shard_t> error_shards;
   for (auto &shard: std::views::keys(read_result.errors)) {
     error_shards.insert(shard);
@@ -334,6 +334,8 @@ int ECCommon::ReadPipeline::get_remaining_shards(
     return -EIO;
   }
 
+  bool need_attr_request = want_attrs;
+
   // Rather than repeating whole read, we can remove everything we already have.
   for (auto iter = read_request.shard_reads.begin();
        iter != read_request.shard_reads.end();) {
@@ -350,7 +352,33 @@ int ECCommon::ReadPipeline::get_remaining_shards(
     if (do_erase) {
       iter = read_request.shard_reads.erase(iter);
     } else {
+      if (need_attr_request && !sinfo.is_nonprimary_shard(shard_id)) {
+        // We have a suitable candidate for attr requests.
+        need_attr_request = false;
+      }
       ++iter;
+    }
+  }
+
+  if (need_attr_request) {
+    // This happens if we got an error from the shard where we were requesting
+    // the attributes from and the recovery does not require any non primary
+    // shards. The example seen in test was a 2+1 EC being recovered. shards 0
+    // and 2 were being requested and read as part of recovery. Shard was reading
+    // the attributes and failed. The recovery required shard 1, but that does
+    // not have valid attributes on it, so the attribute read failed.
+    // This is a pretty obscure case, so no need to optimise that much. Do an
+    // empty read!
+    shard_id_set have;
+    shard_id_map<pg_shard_t> pg_shards(sinfo.get_k_plus_m());
+    get_all_avail_shards(hoid, have, pg_shards, for_recovery, error_shards);
+    for (auto shard : have) {
+      if (!sinfo.is_nonprimary_shard(shard)) {
+        shard_read_t shard_read;
+        shard_read.pg_shard = pg_shards[shard];
+        read_request.shard_reads.insert(shard, shard_read_t());
+        break;
+      }
     }
   }
 
@@ -615,9 +643,8 @@ int ECCommon::ReadPipeline::send_all_remaining_reads(
   read_request_t &read_request = rop.to_read.at(hoid);
   // reset the old shard reads, we are going to read them again.
   read_request.shard_reads.clear();
-  read_request.want_attrs = want_attrs;
   return get_remaining_shards(hoid, rop.complete.at(hoid), read_request,
-                              rop.for_recovery);
+                              rop.for_recovery, want_attrs);
 }
 
 void ECCommon::ReadPipeline::kick_reads() {
