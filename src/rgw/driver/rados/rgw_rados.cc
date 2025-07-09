2a130089aa8643b138872e09ef37395712f096d2
Matt Benjamin <mbenjamin@redhat.com>
2025-07-09 11:11:45 -0400

    rgw: hook up delete-marker detection
    Introduces is_delete_marker() getter in rgw::sal::Object interface,
    and implements it for rgw/sal/rados (and filter), replacing
    unimplemented Object::get_delete_marker().
    
    (Looked into
    rgw/sal/posix, but delete marker handling seems not working as expected
    there, so deferring for now.)
    
    Next, the new interface is used in GET/HEAD request paths to return
    a correct AWS-specified x-amz-delete-marker header when required, c.f.
    https://docs.aws.amazon.com/AmazonS3/latest/userguide/DeleteMarker.html.
    
    Fixes: https://tracker.ceph.com/issues/70853
    Resolves: IBMCEPH-13828
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 8654b1f202ad5719901dbcd8c64a384a75a08adc)

--- src/rgw/driver/rados/rgw_rados.cc
+++ src/rgw/driver/rados/rgw_rados.cc
@@ -7150,11 +7150,11 @@ int RGWRados::get_obj_state(const DoutPrefixProvider *dpp, RGWObjectCtx *rctx,
   RGWObjStateManifest* sm = nullptr;
   int r = get_obj_state(dpp, rctx, bucket_info, obj, &sm,
                         follow_olh, y, assume_noent);
-  if (r < 0) {
-    return r;
-  }
   if (pstate) {
     *pstate = &sm->state;
+  }
+  if (r < 0) {
+    return r;
   }
   if (pmanifest) {
     if (sm->manifest) {
@@ -9862,6 +9862,8 @@ int RGWRados::follow_olh(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_in
   }
 
   if (olh.removed) {
+    /* the object is a delete marker */
+    state->is_dm = true;
     return -ENOENT;
   }
 
