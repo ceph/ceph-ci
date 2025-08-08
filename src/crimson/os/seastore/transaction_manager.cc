529263a114a1992cfe87cefaf56bb1ccfca8c8dd
Zhang Song <zhangsong02@qianxin.com>
2025-08-08 16:58:22 +0800

    crimson/os/seastore: make cold rbm cleaner use different metric prefix
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/transaction_manager.cc
+++ src/crimson/os/seastore/transaction_manager.cc
@@ -1541,7 +1541,8 @@ TransactionManagerRef make_transaction_manager(
       std::move(cold_rbs),
       *backref_manager,
       *lba_manager,
-      cleaner_is_detailed);
+      cleaner_is_detailed,
+      true);
   }
 
   if (backend_type == backend_type_t::SEGMENTED) {
@@ -1569,7 +1570,8 @@ TransactionManagerRef make_transaction_manager(
       std::move(rbs),
       *backref_manager,
       *lba_manager,
-      cleaner_is_detailed);
+      cleaner_is_detailed,
+      false);
     journal = journal::make_circularbounded(
       store_index,
       *journal_trimmer,
