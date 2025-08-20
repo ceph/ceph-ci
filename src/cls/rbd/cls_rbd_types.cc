6468a3901d24a5d742172e4803af7f42d2adfdbc
VinayBhaskar-V <vvarada@redhat.com>
2025-08-20 18:30:09 +0000

    rbd-mirror: integration of the new GroupSnapshotNamespaceMirror::complete field
    This commit introduces the new field **complete**, of type **MirrorGroupSnapshotCompleteState** enum,
    to the GroupSnapshotNamespaceMirror structure. This change is necessary to align behavior of
    mirror group snapshots with that of mirror image snapshots, allowing for a precise differentiation
    between a group snapshot that has been created and one that has been fully synced.
    
    **1. Handling Old-Style Snapshots**
    
    Decoding Old Snapshots: The original GroupSnapshotNamespaceMirror structure lacked the complete field,
    which implicitly defaulted to a bool value of false upon initialization.
    When an old snapshot (lacking the complete field) is decoded by an upgraded client,
    the implicit default value maps to MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED.
    
    Completion Check: A snapshot is determined old by checking it's complete filed i.e
    complete == MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED and if it's old the sync completion
    for these group snapshots is determined by checking the state field
    i.e state == GROUP_SNAPSHOT_STATE_CREATED.
    
    During a upgrade where **OSDs have not yet been updated**, the new client will be forced to create
    snapshots using the old style. These snapshots will be initialized with MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED
    and will stay on that to prevent immediate, incorrect cleanup by the old OSDs and in this case
    state field is set to **GROUP_SNAPSHOT_STATE_CREATED** only after snapshot completed it's sync.
    
    **2. Handling New-Style Snapshots**
    
    New snapshots are initialized with complete == **MIRROR_GROUP_SNAPSHOT_INCOMPLETE**,
    state == GROUP_SNAPSHOT_STATE_CREATING. The group snapshot's state is marked as GROUP_SNAPSHOT_STATE_CREATED
    as soon as its metadata is fully available and stored.
    
    Completion Check: The snapshot's sync is confirmed only when complete == MIRROR_GROUP_SNAPSHOT_COMPLETE
    along with state check (state == GROUP_SNAPSHOT_STATE_CREATED) is satisfied.
    
    This approach ensures seamless transition and compatibility, allowing the system to correctly interpret the
    synchronization status of both old and newly created group snapshots.
    
    Signed-off-by: VinayBhaskar-V <vvarada@redhat.com>
    
    Resolves: rhbz#2396583

--- src/cls/rbd/cls_rbd_types.cc
+++ src/cls/rbd/cls_rbd_types.cc
@@ -1465,6 +1465,24 @@ std::ostream& operator<<(std::ostream& os, MirrorSnapshotState state) {
   return os;
 }
 
+std::ostream& operator<<(std::ostream& os, MirrorGroupSnapshotCompleteState state) {
+  switch (state) {
+  case MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED:
+    os << "complete_if_created";
+    break;
+  case MIRROR_GROUP_SNAPSHOT_INCOMPLETE:
+    os << "incomplete";
+    break;
+  case MIRROR_GROUP_SNAPSHOT_COMPLETE:
+    os << "complete";
+    break;
+  default:
+    os << "unknown (" << static_cast<uint32_t>(state) << ")";
+    break;
+  }
+  return os;
+}
+
 void GroupSnapshotNamespaceMirror::encode(bufferlist& bl) const {
   using ceph::encode;
   encode(state, bl);
@@ -1486,7 +1504,7 @@ void GroupSnapshotNamespaceMirror::decode(uint8_t version,
 
 void GroupSnapshotNamespaceMirror::dump(Formatter *f) const {
   f->dump_stream("state") << state;
-  f->dump_bool("complete", complete);
+  f->dump_stream("complete") << complete;
   f->open_array_section("mirror_peer_uuids");
   for (auto &peer : mirror_peer_uuids) {
     f->dump_string("mirror_peer_uuid", peer);
@@ -1599,7 +1617,8 @@ void GroupSnapshotNamespace::generate_test_instances(
   o.push_back(new GroupSnapshotNamespace(GroupSnapshotNamespaceUser()));
   o.push_back(new GroupSnapshotNamespace(GroupSnapshotNamespaceMirror(
                                              MIRROR_SNAPSHOT_STATE_PRIMARY,
-                                             {"peer uuid"}, "", "")));
+                                             {"peer uuid"}, "", "",
+                                             MIRROR_GROUP_SNAPSHOT_COMPLETE)));
 }
 
 std::ostream& operator<<(std::ostream& os, const GroupSnapshotNamespaceType& type) {
@@ -1644,11 +1663,11 @@ std::ostream& operator<<(std::ostream& os, const GroupSnapshotNamespaceUnknown& 
 
 std::ostream& operator<<(std::ostream& os, GroupSnapshotState state) {
   switch (state) {
-  case GROUP_SNAPSHOT_STATE_INCOMPLETE:
-    os << "incomplete";
+  case GROUP_SNAPSHOT_STATE_CREATING:
+    os << "creating";
     break;
-  case GROUP_SNAPSHOT_STATE_COMPLETE:
-    os << "complete";
+  case GROUP_SNAPSHOT_STATE_CREATED:
+    os << "created";
     break;
   default:
     os << "unknown (" << static_cast<uint32_t>(state) << ")";
@@ -1727,13 +1746,13 @@ void GroupSnapshot::dump(Formatter *f) const {
 void GroupSnapshot::generate_test_instances(std::list<GroupSnapshot *> &o) {
   o.push_back(new GroupSnapshot("10152ae8944a", GroupSnapshotNamespaceUser{},
                                 "groupsnapshot1",
-                                GROUP_SNAPSHOT_STATE_INCOMPLETE));
+                                GROUP_SNAPSHOT_STATE_CREATING));
   o.push_back(new GroupSnapshot("1018643c9869",
                                 GroupSnapshotNamespaceMirror{
                                     MIRROR_SNAPSHOT_STATE_NON_PRIMARY, {},
-                                    "uuid", "id"},
+                                    "uuid", "id", MIRROR_GROUP_SNAPSHOT_COMPLETE},
                                 "groupsnapshot2",
-                                GROUP_SNAPSHOT_STATE_COMPLETE));
+                                GROUP_SNAPSHOT_STATE_CREATED));
 }
 
 void TrashImageSpec::encode(bufferlist& bl) const {
@@ -1990,6 +2009,47 @@ void sanitize_entity_inst(entity_inst_t* entity_inst) {
   // identifies them and clients and on-disk formats can be encoded
   // with different backwards compatibility settings.
   entity_inst->addr.set_type(entity_addr_t::TYPE_ANY);
+}
+
+bool is_mirror_group_snapshot_complete(
+    const cls::rbd::GroupSnapshotState &group_snap_state,
+    const cls::rbd::MirrorGroupSnapshotCompleteState &complete) {
+  // complete -- mirror_ns->complete is used
+  if (complete == cls::rbd::MIRROR_GROUP_SNAPSHOT_COMPLETE) {
+    ceph_assert(group_snap_state == cls::rbd::GROUP_SNAPSHOT_STATE_CREATED);
+    return true;
+  }
+
+  // complete -- mirror_ns->complete is not used (backwards compatibility)
+  if (complete == cls::rbd::MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED &&
+      group_snap_state == cls::rbd::GROUP_SNAPSHOT_STATE_CREATED) {
+    return true;
+  }
+
+  return false;
+}
+
+cls::rbd::MirrorGroupSnapshotCompleteState
+get_mirror_group_snapshot_complete_initial(int8_t require_osd_release) {
+  if (require_osd_release >= CEPH_RELEASE_TENTACLE) {
+    // a new-style mirror group snapshot (i.e. first
+    // MIRROR_GROUP_SNAPSHOT_INCOMPLETE, later transitions to
+    // MIRROR_GROUP_SNAPSHOT_COMPLETE)
+    return cls::rbd::MIRROR_GROUP_SNAPSHOT_INCOMPLETE;
+  } else {
+    // an old-style mirror group snapshot (i.e. stays in
+    // MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED)
+    return cls::rbd::MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED;
+  }
+}
+
+void set_mirror_group_snapshot_complete(cls::rbd::GroupSnapshot& group_snap) {
+  auto& mirror_ns = std::get<cls::rbd::GroupSnapshotNamespaceMirror>(
+      group_snap.snapshot_namespace);
+  if (mirror_ns.complete != cls::rbd::MIRROR_GROUP_SNAPSHOT_COMPLETE_IF_CREATED) {
+    ceph_assert(group_snap.state == cls::rbd::GROUP_SNAPSHOT_STATE_CREATED);
+    mirror_ns.complete = cls::rbd::MIRROR_GROUP_SNAPSHOT_COMPLETE;
+  }
 }
 
 } // namespace rbd
