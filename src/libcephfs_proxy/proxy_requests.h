062951b445dd95be4452b167eb14b436963c86cd
Xavi Hernandez <xhernandez@gmail.com>
2025-09-16 10:24:44 +0200

    libcephfs_proxy: add protocol definitions for nonblocking fsync
    Resolves: IBMCEPH-15573
    Signed-off-by: Xavi Hernandez <xhernandez@gmail.com>
    (cherry picked from commit 0c25e5b8f8ef1ec65dd285b3daad907d536d8541)
    (cherry picked from commit 1f0b9bb66002ed88170af173dcca440014afdb00)

--- src/libcephfs_proxy/proxy_requests.h
+++ src/libcephfs_proxy/proxy_requests.h
@@ -134,6 +134,7 @@ enum {
 	LIBCEPHFSD_OP_LL_SET_FSCRYPT_POLICY_V2,
 	LIBCEPHFSD_OP_LL_GET_FSCRYPT_POLICY_V2,
 	LIBCEPHFSD_OP_LL_IS_ENCRYPTED,
+	LIBCEPHFSD_OP_LL_NONBLOCKING_FSYNC,
 
 	/* Add more operations above this comment. */
 
@@ -143,6 +144,7 @@ enum {
 enum {
 	LIBCEPHFSD_CBK_NULL = 0,
 	LIBCEPHFSD_CBK_LL_NONBLOCKING_RW,
+	LIBCEPHFSD_CBK_LL_NONBLOCKING_FSYNC,
 
 	/* Add more callbacks above this comment. */
 
@@ -1082,6 +1084,22 @@ PROTO_CALL(ceph_ll_is_encrypted,
 	)
 );
 
+PROTO_CALL(ceph_ll_nonblocking_fsync,
+	PROTO_REQ(
+		PROTO_VER(v0,
+			uint64_t cmount;
+			uint64_t info;
+			uint64_t inode;
+			bool syncdataonly;
+		)
+	),
+	PROTO_ANS(
+		PROTO_VER(v0,
+			int32_t res;
+		)
+	)
+);
+
 typedef union _proxy_req {
 	proxy_link_req_t header;
 
@@ -1139,6 +1157,7 @@ typedef union _proxy_req {
 	proxy_ceph_ll_set_fscrypt_policy_v2_req_t ll_set_fscrypt_policy_v2;
 	proxy_ceph_ll_get_fscrypt_policy_v2_req_t ll_get_fscrypt_policy_v2;
 	proxy_ceph_ll_is_encrypted_req_t ll_is_encrypted;
+	proxy_ceph_ll_nonblocking_fsync_req_t ll_nonblocking_fsync;
 } proxy_req_t;
 
 PROTO_NOTIFY(ceph_ll_nonblocking_readv_writev,
@@ -1150,9 +1169,19 @@ PROTO_NOTIFY(ceph_ll_nonblocking_readv_writev,
 	)
 );
 
+PROTO_NOTIFY(ceph_ll_nonblocking_fsync,
+	PROTO_CBK(
+		PROTO_VER(v0,
+			uint64_t info;
+			int64_t res;
+		)
+	)
+);
+
 typedef union _proxy_cbk {
 	proxy_link_req_t header;
 	proxy_ceph_ll_nonblocking_readv_writev_cbk_t ll_nonblocking_rw;
+	proxy_ceph_ll_nonblocking_fsync_cbk_t ll_nonblocking_fsync;
 } proxy_cbk_t;
 
 #endif
