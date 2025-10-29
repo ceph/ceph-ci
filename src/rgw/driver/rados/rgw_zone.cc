b00b5e96a97542e8701b2d94a4d19e36606db722
Soumya Koduri <skoduri@redhat.com>
2025-10-30 01:30:11 +0530

    rgw/cloud-transition: Include LocationConstraint for non-default regions
    Add a new tier-config option "location_constraint" to be configured
    while using AWS non-default regions.
    
    Resolves: rhbz#2402662
    
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>
    (cherry picked from commit 7bca6467c783a7f33209721ea20d728e58daf457)

--- src/rgw/driver/rados/rgw_zone.cc
+++ src/rgw/driver/rados/rgw_zone.cc
@@ -1433,6 +1433,9 @@ int RGWZoneGroupPlacementTierS3::update_params(const JSONFormattable& config)
       host_style = VirtualStyle;
     }
   }
+  if (config.exists("location_constraint")) {
+    location_constraint = config["location_constraint"];
+  }
   if (config.exists("target_storage_class")) {
     target_storage_class = config["target_storage_class"];
   }
@@ -1491,6 +1494,9 @@ int RGWZoneGroupPlacementTierS3::clear_params(const JSONFormattable& config)
   if (config.exists("host_style")) {
     /* default */
     host_style = PathStyle;
+  }
+  if (config.exists("location_constraint")) {
+    location_constraint.clear();
   }
   if (config.exists("target_storage_class")) {
     target_storage_class.clear();
