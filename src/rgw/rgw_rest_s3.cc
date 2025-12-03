309926d452612faadf71d9f3fc4c311012eca3f8
Ali Masarwa <amasarwa@redhat.com>
2025-12-03 19:19:09 +0200

    RGW: add support global CORS rule
    Resolves: ISCE-2697
    
    Signed-off-by: Ali Masarwa <amasarwa@redhat.com>
    (cherry picked from commit b504ae3dd80eea6a4ce3b2e887843211ebdaf35f)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -1651,7 +1651,7 @@ void RGWListBuckets_ObjStore_S3::send_response_begin(bool has_buckets)
   dump_start(s);
   // Explicitly use chunked transfer encoding so that we can stream the result
   // to the user without having to wait for the full length of it.
-  end_header(s, NULL, to_mime_type(s->format), CHUNKED_TRANSFER_ENCODING);
+  end_header(s, this, to_mime_type(s->format), CHUNKED_TRANSFER_ENCODING);
 
   if (! op_ret) {
     list_all_buckets_start(s);
@@ -2741,7 +2741,7 @@ void RGWCreateBucket_ObjStore_S3::send_response()
     set_req_state_err(s, op_ret);
   }
   dump_errno(s);
-  end_header(s);
+  end_header(s, this);
 
   if (op_ret < 0)
     return;
@@ -4406,7 +4406,7 @@ void RGWPutBucketEncryption_ObjStore_S3::send_response()
     set_req_state_err(s, op_ret);
   }
   dump_errno(s);
-  end_header(s);
+  end_header(s, this);
 }
 
 void RGWGetBucketEncryption_ObjStore_S3::send_response()
@@ -4437,7 +4437,7 @@ void RGWDeleteBucketEncryption_ObjStore_S3::send_response()
 
   set_req_state_err(s, op_ret);
   dump_errno(s);
-  end_header(s);
+  end_header(s,this);
 }
 
 void RGWGetRequestPayment_ObjStore_S3::send_response()
@@ -5068,7 +5068,7 @@ void RGWPutBucketObjectLock_ObjStore_S3::send_response()
     set_req_state_err(s, op_ret);
   }
   dump_errno(s);
-  end_header(s);
+  end_header(s, this);
 }
 
 void RGWGetBucketObjectLock_ObjStore_S3::send_response()
@@ -5200,6 +5200,11 @@ RGWOp *RGWHandler_REST_Service_S3::op_get()
   } else {
     return new RGWListBuckets_ObjStore_S3;
   }
+}
+
+RGWOp *RGWHandler_REST_Service_S3::op_options()
+{
+  return new RGWOptionsCORS_ObjStore_S3;
 }
 
 RGWOp *RGWHandler_REST_Service_S3::op_head()
