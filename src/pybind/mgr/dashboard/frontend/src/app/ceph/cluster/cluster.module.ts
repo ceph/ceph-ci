2faf7d36b4aed92c75390f60d411a525cad53ac3
Abhishek Desai <abhishek.desai1@ibm.com>
2026-01-19 14:17:54 +0530

    mgr/dashboard : Add Certificate tab under service details Resolves : ISCE-3321 fixes : https://tracker.ceph.com/issues/74429 Signed-off-by: Abhishek Desai <abhishek.desai1@ibm.com>
    (cherry picked from commit 8055730e19c904eff2a31992a0a6ce51318ddba0)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/cluster.module.ts

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/cluster.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/cluster.module.ts
@@ -24,7 +24,8 @@ import {
   RadioModule,
   TilesModule,
   LayerModule,
-  ProgressBarModule
+  ProgressBarModule,
+  TabsModule
 } from 'carbon-components-angular';
 
 import {
@@ -77,6 +78,7 @@ import { SilenceListComponent } from './prometheus/silence-list/silence-list.com
 import { SilenceMatcherModalComponent } from './prometheus/silence-matcher-modal/silence-matcher-modal.component';
 import { PlacementPipe } from './services/placement.pipe';
 import { ServiceDaemonListComponent } from './services/service-daemon-list/service-daemon-list.component';
+import { ServiceCertificateDetailsComponent } from './services/service-cert-details/service-certificate-details.component';
 import { ServiceDetailsComponent } from './services/service-details/service-details.component';
 import { ServiceFormComponent } from './services/service-form/service-form.component';
 import { ServicesComponent } from './services/services.component';
@@ -125,6 +127,7 @@ import { LicenceAgreementComponent } from './license-agreement/license-agreement
     ToggletipModule,
     IconModule,
     TagModule,
+    TabsModule,
     TextLabelListComponent,
     SelectModule,
     LayoutModule,
@@ -166,6 +169,7 @@ import { LicenceAgreementComponent } from './license-agreement/license-agreement
     ActiveAlertListComponent,
     ServiceDetailsComponent,
     ServiceDaemonListComponent,
+    ServiceCertificateDetailsComponent,
     TelemetryComponent,
     PrometheusTabsComponent,
     ServiceFormComponent,
