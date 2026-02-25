988fab91c3311e075b9079109201788faabeb1f4
Krunal Chheda <kchheda3@bloomberg.net>
2026-02-25 09:53:00 -0500

    rgw/oidc: add rgwrados::oidc interface to support multisite.
    add a new interface for oidc metadata that is used by both RadosStore and the
    oidc metadata handler for replication.
    
    Signed-off-by: Krunal Chheda <kchheda3@bloomberg.net>
    (cherry picked from commit f35001b4aa265442730f1c77d2a25094d4fbaa69)

--- src/rgw/driver/rados/rgw_service.h
+++ src/rgw/driver/rados/rgw_service.h
@@ -184,6 +184,7 @@ struct RGWCtlDef {
     std::unique_ptr<RGWMetadataHandler> user;
     std::unique_ptr<RGWMetadataHandler> otp;
     std::unique_ptr<RGWMetadataHandler> role;
+    std::unique_ptr<RGWMetadataHandler> oidc;
     std::unique_ptr<RGWMetadataHandler> topic;
     std::unique_ptr<RGWMetadataHandler> account;
     std::unique_ptr<RGWMetadataHandler> group;
