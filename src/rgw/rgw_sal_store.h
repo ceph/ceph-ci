24794e32867865dcfc72c736f9957b32739f1741
Casey Bodley <cbodley@redhat.com>
2026-03-12 15:44:22 -0400

    rgw/sal: StoreMPSerializer uses atomic bool locked
    make it safe for other threads to call clear_locked() and is_locked()
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 540b8e0ed6ddcdf0c1a76457855db0a37ad970a3)
    Resolves: IBMCEPH-13341

--- src/rgw/rgw_sal_store.h
+++ src/rgw/rgw_sal_store.h
@@ -429,7 +429,7 @@ public:
 
 class StoreMPSerializer : public MPSerializer {
 protected:
-  bool locked;
+  std::atomic<bool> locked;
   std::string oid;
 public:
   StoreMPSerializer() : locked(false) {}
