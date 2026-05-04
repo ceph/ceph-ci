20a9e1e73b146dd12377216aa910c4513e88fe3a
Shilpa Jagannath <smanjara@redhat.com>
2026-05-04 16:16:35 -0400

    rgw: fix assert crash in RGWDeleteMultiObj end_response()
    remove the close_section() call from end_response() and rely
    on output_footer() inside rgw_flush_formatter_and_reset() to close the
    DeleteResult section safely.
    
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>
    (cherry picked from commit 038b6df0cadabcd5d23cea01b86e3b3587a481e4)
    Resolves: IBMCEPH-14213
    (cherry picked from commit 64a546a5a05b230be2e3f2f4b1751e0240f18a34)

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -4981,7 +4981,6 @@ void RGWDeleteMultiObj_ObjStore_S3::send_partial_response(const rgw_obj_key& key
 
 void RGWDeleteMultiObj_ObjStore_S3::end_response()
 {
-  s->formatter->close_section();
   rgw_flush_formatter_and_reset(s, s->formatter);
 }
 
