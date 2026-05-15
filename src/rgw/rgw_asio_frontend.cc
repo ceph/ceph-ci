45e35f49b778555666ae8d8ae655944bd6daee5f
Casey Bodley <cbodley@redhat.com>
2026-05-15 10:40:50 -0400

    rgw/beast: add ssl_ciphersuites option for tls 1.3
    the existing ssl_ciphers option is passed to `SSL_CTX_set_cipher_list()`
    which only applies to "TLSv1.2 and below". there's a separate
    `SSL_CTX_set_ciphersuites()` for TLSv1.3
    
    because the frontend's default configuration for `ssl_options` accepts
    both 1.2 and 1.3, users may need to specify ciphers for each. that's why
    `ssl_ciphersuites` is introduced as a separate option
    
    Fixes: https://tracker.ceph.com/issues/76578
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit c62f537f2c99513ef04595c748d392e9da36a7fd)
    Resolves: IBMCEPH-14378
    
    Conflicts:
    	doc/radosgw/frontends.rst  trivial "openssl" -> "OpenSSL"
    	src/rgw/rgw_asio_frontend.cc  trivial "ssl_ctx" -> "ssl_context"

--- src/rgw/rgw_asio_frontend.cc
+++ src/rgw/rgw_asio_frontend.cc
@@ -1044,6 +1044,21 @@ int AsioFrontend::init_ssl()
     }
   }
 
+  std::optional<string> ciphersuites = conf->get_val("ssl_ciphersuites");
+  if (ciphersuites) {
+    if (!cert) {
+      lderr(ctx()) << "no ssl_certificate configured for ssl_ciphersuites" << dendl;
+      return -EINVAL;
+    }
+
+    int r = SSL_CTX_set_ciphersuites(ssl_context->native_handle(), ciphersuites->c_str());
+    if (r == 0) {
+      lderr(ctx()) << "no cipher could be selected from ssl_ciphersuites: "
+                   << *ciphersuites << dendl;
+      return -EINVAL;
+    }
+  }
+
   std::optional<std::string> groups = conf->get_val("tls_groups");
   if (groups) {
     if (!cert) {
