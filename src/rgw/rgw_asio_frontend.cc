rhcs-dashboard/ceph

rgw/beast: add frontend option 'tls_groups'
allow frontend config to specify a list of strings for openssl
SSL_CTX_set1_groups_list()

Fixes: https://tracker.ceph.com/issues/75568

Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_asio_frontend.cc
@@ -1072,6 +1072,20 @@ int AsioFrontend::ssl_reload() {
     }
   }
 
+  std::optional<std::string> groups = conf->get_val("tls_groups");
+  if (groups) {
+    if (!cert) {
+      lderr(ctx()) << "no ssl_certificate configured for tls_groups" << dendl;
+      return -EINVAL;
+    }
+
+    int r = SSL_CTX_set1_groups_list(ssl_ctx->native_handle(), groups->c_str());
+    if (r == 0) {
+      lderr(ctx()) << "openssl rejected tls_groups: " << *groups << dendl;
+      return -EINVAL;
+    }
+  }
+
   bool key_is_cert = false;
   bool have_cert = false;
   if (cert) {

ceph/ceph-ci

(cherry picked from commit a9c929055f8bd047dd9d8b4ddadb2006b7b40fcd)
Resolves: ISCE-3869
Conflicts:
	doc/radosgw/frontends.rst  extra link to config-ref.rst
	src/rgw/rgw_asio_frontend.cc  missing cert reload

src/rgw/rgw_asio_frontend.cc
@@ -1044,6 +1044,20 @@ int AsioFrontend::init_ssl()
     }
   }
 
+  std::optional<std::string> groups = conf->get_val("tls_groups");
+  if (groups) {
+    if (!cert) {
+      lderr(ctx()) << "no ssl_certificate configured for tls_groups" << dendl;
+      return -EINVAL;
+    }
+
+    int r = SSL_CTX_set1_groups_list(ssl_context->native_handle(), groups->c_str());
+    if (r == 0) {
+      lderr(ctx()) << "openssl rejected tls_groups: " << *groups << dendl;
+      return -EINVAL;
+    }
+  }
+
   auto ports = config.equal_range("ssl_port");
   auto endpoints = config.equal_range("ssl_endpoint");
 