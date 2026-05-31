bcfa0e97f82f2ec0c9c0102416469d754e214439
Timothy Q Nguyen <timqn22@gmail.com>
2026-05-31 00:15:46 -0400

    python-common: multi-line for yaml
    Currently when running the command ceph orch ls --export
    multi-line strings are processed using default interpretation
    meaning they could be printed with quotations instead of
    the expected | . This causes a hassle for users who want to
    copy their specifications to new yaml files as they have to
    manually modify the yaml to remove the quotes and add | .
    My code fixes this by modifying the yaml representer to
    check for multi-line strings and wrapping them in a
    YamlLiteralString class which will mark the multi-line string
    to be processed with | . I've also added unit tests.
    
    Signed-off-by: Timothy Q Nguyen <timqn22@gmail.com>
    (cherry picked from commit 0a73f686c8734e6545fef48275af56502300a4e7)

--- src/python-common/ceph/tests/test_service_spec.py
+++ src/python-common/ceph/tests/test_service_spec.py
@@ -19,6 +19,7 @@ from ceph.deployment.service_spec import (
     PrometheusSpec,
     RGWSpec,
     ServiceSpec,
+    YamlLiteralString,
 )
 from ceph.deployment.drive_group import DriveGroupSpec
 from ceph.deployment.hostspec import SpecValidationError
@@ -1532,3 +1533,37 @@ def test_non_osd_services_do_not_get_default_termination_if_not_provided(spec_da
 
     spec_section = j.get('spec', {})
     assert 'termination_grace_period_seconds' not in spec_section
+
+def test_yaml_literal_string_class_represents_multiline_strings_as_literal():
+    multiline_string = "test1\ntest2\n"
+    dumped = yaml.dump(YamlLiteralString(multiline_string))
+
+    assert dumped.startswith('|')
+
+def test_yaml_representer_can_handle_multiline_strings_for_export():
+    spec_data = """
+service_type: iscsi
+service_id: iscsi
+placement:
+  label: iscsi
+spec:
+  pool: testpool
+  ssl_cert: |
+    -----BEGIN CERTIFICATE-----
+    FILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLER
+    FILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLER
+    -----END CERTIFICATE-----
+  ssl_key: |
+    -----BEGIN PRIVATE KEY-----
+    FILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLER
+    FILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLERFILLER
+    -----END PRIVATE KEY-----
+"""
+
+    data = yaml.safe_load(spec_data)
+    spec_obj = ServiceSpec.from_json(data)
+
+    dumped = yaml.dump(spec_obj, default_flow_style=False)
+
+    assert 'ssl_cert: |' in dumped
+    assert 'ssl_key: |' in dumped
