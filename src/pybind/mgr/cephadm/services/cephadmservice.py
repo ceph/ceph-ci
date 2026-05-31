1c0ebf8bbc58fda8e150c5213677f6dea84c4022
Guillaume Abrioux <gabrioux@ibm.com>
2026-05-31 00:15:27 -0400

    cephadm: re-add 'crash' to daemon type list in get_auth_entity()
    aae5ef14644481dc166dce7dca4784f372ddc0fd introduced a regression
    most likely a merge conflict dropped crash daemon from the list by
    mistake.
    
    Resolves: ISCE-3931
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

--- src/pybind/mgr/cephadm/services/cephadmservice.py
+++ src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -63,7 +63,7 @@ def get_auth_entity(daemon_type: str, daemon_id: str, host: str = "") -> AuthEnt
     """
     # despite this mapping entity names to daemons, self.TYPE within
     # the CephService class refers to service types, not daemon types
-    if daemon_type in ['rgw', 'rbd-mirror', 'cephfs-mirror', "iscsi", 'nvmeof', 'ingress', 'ceph-exporter']:
+    if daemon_type in ['rgw', 'rbd-mirror', 'cephfs-mirror', "iscsi", 'nvmeof', 'ingress', 'ceph-exporter', 'crash']:
         return AuthEntity(f'client.{daemon_type}.{daemon_id}')
     elif daemon_type in ['crash', 'agent', 'node-proxy']:
         if host == "":
