ceph/ceph

qa/workunits/smb: add test_simple_smb.py very simple smb tests
Signed-off-by: John Mulligan <jmulligan@redhat.com>

qa/workunits/smb/tests/test_simple_smb.py
@@ -1,0 +1,37 @@
+import pytest
+
+import smbutil
+
+
+@pytest.mark.default
+def test_arbitrary_listdir(smbconf, share_name):
+    with smbutil.connection(smbconf, share_name) as sharep:
+        contents = sharep.listdir()
+        assert isinstance(contents, list)
+
+
+@pytest.mark.default
+def test_create_dir(smbconf, share_name):
+    with smbutil.connection(smbconf, share_name) as sharep:
+        tdir = sharep / 'test_create_dir'
+        tdir.mkdir(exist_ok=True)
+        try:
+            contents = sharep.listdir()
+            assert 'test_create_dir' in contents
+        finally:
+            tdir.rmdir()
+
+
+@pytest.mark.default
+def test_create_file(smbconf, share_name):
+    with smbutil.connection(smbconf, share_name) as sharep:
+        fname = sharep / 'file1.dat'
+        fname.write_text('HELLO WORLD\n')
+        try:
+            contents = sharep.listdir()
+            assert 'file1.dat' in contents
+
+            txt = fname.read_text()
+            assert txt == 'HELLO WORLD\n'
+        finally:
+            fname.unlink()

ceph/ceph-ci

Resolves: IBMCEPH-13332 (prereq)
(cherry picked from commit cf8fff7b98b1cc97097650764ca9b90689922059)

qa/workunits/smb/tests/test_simple_smb.py
@@ -1,0 +1,37 @@
+import pytest
+
+import smbutil
+
+
+@pytest.mark.default
+def test_arbitrary_listdir(smb_cfg, share_name):
+    with smbutil.connection(smb_cfg, share_name) as sharep:
+        contents = sharep.listdir()
+        assert isinstance(contents, list)
+
+
+@pytest.mark.default
+def test_create_dir(smb_cfg, share_name):
+    with smbutil.connection(smb_cfg, share_name) as sharep:
+        tdir = sharep / 'test_create_dir'
+        tdir.mkdir(exist_ok=True)
+        try:
+            contents = sharep.listdir()
+            assert 'test_create_dir' in contents
+        finally:
+            tdir.rmdir()
+
+
+@pytest.mark.default
+def test_create_file(smb_cfg, share_name):
+    with smbutil.connection(smb_cfg, share_name) as sharep:
+        fname = sharep / 'file1.dat'
+        fname.write_text('HELLO WORLD\n')
+        try:
+            contents = sharep.listdir()
+            assert 'file1.dat' in contents
+
+            txt = fname.read_text()
+            assert txt == 'HELLO WORLD\n'
+        finally:
+            fname.unlink()