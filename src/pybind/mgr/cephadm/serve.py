19793779950cc65b54f180763c06a8f33a20ef89
Adam King <adking@redhat.com>
2026-06-03 21:56:35 -0400

    mgr/cephadm: don't reconfig daemons for monmap changes while upgrade is in progress
    Change-Id: 1d8dcb7d-e9f8-4804-904f-2252a18e6dc3
    Signed-off-by: Adam King <adking@redhat.com>

--- src/pybind/mgr/cephadm/serve.py
+++ src/pybind/mgr/cephadm/serve.py
@@ -2106,6 +2106,7 @@ def _ceph_service_next_action(
     mgr: 'CephadmOrchestrator',
     last_config: Optional[datetime.datetime],
 ) -> Optional[str]:
+    action = None
     if daemon_type not in CEPH_TYPES:
         return action
     if last_config is None:
@@ -2114,8 +2115,12 @@ def _ceph_service_next_action(
         return action
 
     if mgr.last_monmap and mgr.last_monmap > last_config:
-        logger.info('Reconfiguring %s (monmap changed)...', name)
-        return 'reconfig'
+        if mgr.upgrade.upgrade_state is not None and not mgr.upgrade.upgrade_state.paused:
+            logger.debug('Skipping reconfig of %s for monmap change (upgrade in progress)' % name)
+        else:
+            logger.info('Reconfiguring %s (monmap changed)...' % name)
+            return 'reconfig'
+
     if mgr.extra_ceph_conf_is_newer(last_config):
         logger.info('Reconfiguring %s (extra config changed)...', name)
         return 'reconfig'
