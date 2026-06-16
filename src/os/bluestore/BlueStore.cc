Jayaprakash-ibm/ceph

os/bluestore: start/stop BlueFS spillover cleaner on config change
Fixes: https://tracker.ceph.com/issues/74319

Signed-off-by: Jaya Prakash <jayaprakash@ibm.com>

src/os/bluestore/BlueStore.cc
@@ -5830,7 +5830,8 @@ std::vector<std::string> BlueStore::get_tracked_keys() const noexcept
     "bluestore_onode_segment_size"s,
     "bluestore_allocator_lookup_policy"s,
     "bluestore_volume_selection_reserved_factor"s,
-    "bluestore_volume_selection_reserved"s
+    "bluestore_volume_selection_reserved"s,
+    "bluefs_spillover_cleaner"s,
   };
 }
 
@@ -5913,6 +5914,15 @@ void BlueStore::handle_conf_change(const ConfigProxy& conf,
     changed.count("bluestore_volume_selection_reserved")) {
     if (bluefs)
       bluefs->update_volume_selector_from_config();
+  }
+  if (changed.count("bluefs_spillover_cleaner")) {
+    if (bluefs) {
+      if(cct->_conf.get_val<bool>("bluefs_spillover_cleaner")) {
+        bluefs->_spillover_cleaner_start();
+      } else {
+        bluefs->_spillover_cleaner_stop();
+      }
+    }
   }
 }
 

ceph/ceph-ci

Fixes: https://tracker.ceph.com/issues/77077
Resolves: https://ibm-ceph.atlassian.net/browse/ISCE-4440
(cherry picked from commit 82e1b425cdf1e9f2a63aeb69829ef1726aa91aa0)
(cherry picked from commit e90b64772d7b8118002e7d728ad10da3c70c1eef)

src/os/bluestore/BlueStore.cc
@@ -5829,7 +5829,8 @@ std::vector<std::string> BlueStore::get_tracked_keys() const noexcept
     "bluestore_warn_on_no_per_pg_omap"s,
     "bluestore_max_defer_interval"s,
     "bluestore_onode_segment_size"s,
-    "bluestore_allocator_lookup_policy"s
+    "bluestore_allocator_lookup_policy"s,
+    "bluefs_spillover_cleaner"s
   };
 }
 
@@ -5907,6 +5908,11 @@ void BlueStore::handle_conf_change(const ConfigProxy& conf,
   }
   if (changed.count("bluestore_allocator_lookup_policy")) {
     _update_allocator_lookup_policy();
+  }
+  if (changed.count("bluefs_spillover_cleaner")) {
+    if (bluefs) {
+      bluefs->update_spillover_cleaner_from_config();
+    }
   }
 }
 
@@ -9532,6 +9538,10 @@ int BlueStore::_mount()
     }
   }
 
+  if (bluefs && cct->_conf.get_val<bool>("bluefs_spillover_cleaner")) {
+    bluefs->spillover_cleaner_start();
+  }
+
   mounted = true;
   return 0;
 }
@@ -9541,6 +9551,10 @@ int BlueStore::umount()
   dout(5) << __func__ << dendl;
   ceph_assert(_kv_only || mounted);
   _osr_drain_all();
+
+  if (bluefs) {
+    bluefs->spillover_cleaner_stop();
+  }
 
   mounted = false;
 