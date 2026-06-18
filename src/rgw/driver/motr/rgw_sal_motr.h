mheler/ceph

rgw/cloud-transition: add cloud delete for cloud-tiered objects
Add asynchronous cloud object deletion when local objects are deleted
from buckets using cloud tier storage classes.

Signed-off-by: Matthew N. Heler <matthew.heler@hotmail.com>

src/rgw/driver/motr/rgw_sal_motr.h
@@ -424,6 +424,7 @@ public:
   virtual bool retain_head_object() { return tier.retain_head_object; }
   virtual bool allow_read_through() { return tier.allow_read_through; }
   virtual uint64_t get_read_through_restore_days() { return tier.read_through_restore_days; }
+  virtual bool delete_with_head_object() { return tier.is_tier_type_s3() ? tier.t.s3.delete_with_head_object : false; }
   RGWZoneGroupPlacementTier& get_rt() { return tier; }
 };
 
@@ -1010,6 +1011,7 @@ class MotrStore : public StoreDriver {
     virtual std::unique_ptr<Lifecycle> get_lifecycle(void) override;
     virtual std::unique_ptr<Restore> get_restore(const int n_objs,
 		   const std::vector<std::string_view>& obj_names) override;
+    virtual std::unique_ptr<CloudDelete> get_cloud_delete(void) override { return nullptr; }
     virtual bool process_expired_objects(const DoutPrefixProvider *dpp, optional_yield y) override;
     virtual std::unique_ptr<Notification> get_notification(rgw::sal::Object* obj, rgw::sal::Object* src_obj,
         req_state* s, rgw::notify::EventType event_type, optional_yield y, const std::string* object_name=nullptr) override;

src/rgw/rgw_sal_dbstore.h
@@ -217,6 +217,7 @@ protected:
     virtual bool allow_read_through() { return tier.allow_read_through; }
     virtual uint64_t get_read_through_restore_days() { return tier.read_through_restore_days; }
     virtual bool retain_head_object() { return tier.retain_head_object; }
+    virtual bool delete_with_head_object() { return tier.is_tier_type_s3() ? tier.t.s3.delete_with_head_object : false; }
     RGWZoneGroupPlacementTier& get_rt() { return tier; }
   };
 
@@ -899,6 +900,7 @@ public:
       virtual int cluster_stat(RGWClusterStat& stats) override;
       virtual std::unique_ptr<Lifecycle> get_lifecycle(void) override;
       virtual std::unique_ptr<Restore> get_restore(void) override;
+      virtual std::unique_ptr<CloudDelete> get_cloud_delete(void) override { return nullptr; }
       virtual bool process_expired_objects(const DoutPrefixProvider *dpp, optional_yield y) override;
 
   virtual std::unique_ptr<Notification> get_notification(

ceph/ceph-ci

When both allow_delete_through and retain_head_object are enabled on an
S3 cloud tier, deleting the local head object enqueues an async delete
of the remote copy. A sharded RADOS FIFO queue holds pending deletes,
and a background thread drains entries with exponential backoff
(15 min to 8 hours, configurable retry window up to 7 days).
Versioned buckets: creating a delete marker does not trigger a remote
delete. Only removing an actual object version does, including lifecycle
expirations. The one exception is versioning-suspended buckets, where a
delete that overwrites the null version also cleans up its remote copy.
(cherry picked from commit 53f455ef0f40147147e570bf2dabcab0ae48c179)

src/rgw/driver/motr/rgw_sal_motr.h
@@ -425,6 +425,7 @@ public:
   virtual bool retain_current_version() { return tier.retain_current_version; }
   virtual bool allow_read_through() { return tier.allow_read_through; }
   virtual uint64_t get_read_through_restore_days() { return tier.read_through_restore_days; }
+  virtual bool allow_delete_through() { return tier.is_tier_type_s3() ? tier.t.s3.allow_delete_through : false; }
   RGWZoneGroupPlacementTier& get_rt() { return tier; }
 };
 
@@ -1011,6 +1012,7 @@ class MotrStore : public StoreDriver {
     virtual std::unique_ptr<Lifecycle> get_lifecycle(void) override;
     virtual std::unique_ptr<Restore> get_restore(const int n_objs,
 		   const std::vector<std::string_view>& obj_names) override;
+    virtual std::unique_ptr<CloudDelete> get_cloud_delete(void) override { return nullptr; }
     virtual bool process_expired_objects(const DoutPrefixProvider *dpp, optional_yield y) override;
     virtual std::unique_ptr<Notification> get_notification(rgw::sal::Object* obj, rgw::sal::Object* src_obj,
         req_state* s, rgw::notify::EventType event_type, optional_yield y, const std::string* object_name=nullptr) override;

src/rgw/rgw_sal_dbstore.h
@@ -218,6 +218,7 @@ protected:
     virtual uint64_t get_read_through_restore_days() { return tier.read_through_restore_days; }
     virtual bool retain_head_object() { return tier.retain_head_object; }
     virtual bool retain_current_version() { return tier.retain_current_version; }
+    virtual bool allow_delete_through() { return tier.is_tier_type_s3() ? tier.t.s3.allow_delete_through : false; }
     RGWZoneGroupPlacementTier& get_rt() { return tier; }
   };
 
@@ -900,6 +901,7 @@ public:
       virtual int cluster_stat(RGWClusterStat& stats) override;
       virtual std::unique_ptr<Lifecycle> get_lifecycle(void) override;
       virtual std::unique_ptr<Restore> get_restore(void) override;
+      virtual std::unique_ptr<CloudDelete> get_cloud_delete(void) override { return nullptr; }
       virtual bool process_expired_objects(const DoutPrefixProvider *dpp, optional_yield y) override;
 
   virtual std::unique_ptr<Notification> get_notification(