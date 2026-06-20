ceph/ceph

mds: enable logging for snap.cc
Signed-off-by: Rishabh Dave <ridave@redhat.com>

src/mds/snap.cc
@@ -15,9 +15,15 @@
 
 #include "snap.h"
 #include "common/Formatter.h"
+#include "common/debug.h"
 
 #include <ostream>
 #include <sstream>
+
+#define dout_context g_ceph_context
+#define dout_subsys ceph_subsys_mds
+#undef dout_prefix
+#define dout_prefix *_dout << "mds.server "
 
 using namespace std;
 /*

ceph/ceph-ci

src/mds/snap.cc
@@ -15,9 +15,15 @@
 
 #include "snap.h"
 #include "common/Formatter.h"
+#include "common/debug.h"
 
 #include <ostream>
 #include <sstream>
+
+#define dout_context g_ceph_context
+#define dout_subsys ceph_subsys_mds
+#undef dout_prefix
+#define dout_prefix *_dout << "mds.snap "
 
 using namespace std;
 /*