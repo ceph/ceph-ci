7985bf4419944265d1ad1f5bc1f74c9c3018646f
Xuehan Xu <xuxuehan@qianxin.com>
2026-06-22 17:48:15 +0800

    crimson/os/seastore/lba: make sure all mappings in the copied range is examined when updating paddrs for the copied mappings
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/btree/fixed_kv_btree.h
+++ src/crimson/os/seastore/btree/fixed_kv_btree.h
@@ -139,6 +139,21 @@ public:
 
     }
 
+    // return true if reached the next mapping and false otherwise
+    bool next_sync(op_context_t c) {
+#ifndef NDEBUG
+      assert_valid();
+#endif
+      assert(is_full());
+      assert(!is_end());
+      leaf.pos++;
+      if (at_boundary()) {
+        return handle_boundary_sync(c);
+      } else {
+        return true;
+      }
+    }
+
     iterator_fut prev(op_context_t c) const
     {
 #ifndef NDEBUG
@@ -395,6 +410,44 @@ public:
       });
     }
 
+    // return true if reached the next mapping and false otherwise
+    bool handle_boundary_sync(op_context_t c) {
+      assert(at_boundary());
+      assert(is_full());
+      depth_t depth = 2;
+      for (; depth <= get_depth(); depth++) {
+        auto &internal = get_internal(depth);
+        auto it = internal.node->iter_idx(internal.pos + 1);
+        if (it != internal.node->end()) {
+          auto pos = internal.pos + 1;
+          while (depth > 2) {
+            it = get_internal(depth).node->iter_idx(pos);
+            auto child = get_internal(depth).node->template get_child_sync<
+              internal_node_t>(c.trans, c.cache, pos, it.get_key());
+            if (!is_valid_child_ptr(child.get())) {
+              return false;
+            }
+            depth--;
+            get_internal(depth).node = child;
+            get_internal(depth).pos = pos;
+            pos = 0;
+          }
+          assert(depth == 2);
+          assert(pos == 0);
+          it = get_internal(depth).node->begin();
+          auto child = get_internal(depth).node->template get_child_sync<
+            leaf_node_t>(c.trans, c.cache, pos, it.get_key());
+          if (!is_valid_child_ptr(child.get())) {
+            return false;
+          }
+          leaf.node = child;
+          leaf.pos = 0;
+          return true;
+        }
+      }
+      return false;
+    }
+
     using handle_boundary_ertr = base_iertr;
     using handle_boundary_ret = handle_boundary_ertr::future<>;
     handle_boundary_ret handle_boundary(
@@ -591,10 +644,12 @@ public:
       if (depth > 1) {
         auto child = entry.node->template get_child_sync<internal_node_t>(
           c.trans, c.cache, entry.pos, riter.get_key());
+        ceph_assert(is_valid_child_ptr(child.get()));
         iter.get_internal(depth).node = child;
       } else {
         auto child = entry.node->template get_child_sync<leaf_node_t>(
           c.trans, c.cache, entry.pos, riter.get_key());
+        ceph_assert(is_valid_child_ptr(child.get()));
         iter.leaf.node = child;
       }
     }
