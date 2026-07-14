cbodley/ceph

common/async: add RadosLockClient for with_lease()
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/test/common/test_async_lease.cc
@@ -12,6 +12,7 @@
 
 //#define BOOST_ASIO_ENABLE_HANDLER_TRACKING
 #include "common/async/lease.h"
+#include "common/async/lease_rados.h"
 
 #include <optional>
 #include <utility>

ceph/ceph-ci

src/test/common/test_async_lease.cc
@@ -12,6 +12,7 @@
 
 //#define BOOST_ASIO_ENABLE_HANDLER_TRACKING
 #include "common/async/lease.h"
+#include "common/async/lock_rados.h"
 
 #include <optional>
 #include <utility>