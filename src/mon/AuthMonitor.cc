mkogan1/ceph

mon/AuthMonitor: adjust AUTH_INSECURE_ROTATING_SERVICE_KEY_TYPE to WRN
This is expected to resolve on its own as the older key type rotates
out. A health ERR implies action is required. It's fine to allow the
keys to rotate out within ~3 hours (the default). Wiping the rotating
keys can be disruptive to legacy clients because they won't know their
tickets have been invalidated.

Resolves: IBMCEPH-16410
Change-Id: 90d517b3-6bd2-4332-98d1-878849706bdc
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

src/mon/AuthMonitor.cc
@@ -641,7 +641,7 @@ bool AuthMonitor::check_health()
   if (!bad_rotating_service_keys.empty()) {
     std::ostringstream summary;
     summary << bad_rotating_service_keys.size() << " rotating auth service keys using insecure key types";
-    auto& check = next.add("AUTH_INSECURE_ROTATING_SERVICE_KEY_TYPE", HEALTH_ERR, summary.str(), bad_rotating_service_keys.size());
+    auto& check = next.add("AUTH_INSECURE_ROTATING_SERVICE_KEY_TYPE", HEALTH_WRN, summary.str(), bad_rotating_service_keys.size());
     for (auto& detail : bad_rotating_service_keys) {
       check.detail.push_back(detail);
     }

ceph/ceph-ci

src/mon/AuthMonitor.cc
@@ -643,7 +643,7 @@ bool AuthMonitor::check_health()
   if (!bad_rotating_service_keys.empty()) {
     std::ostringstream summary;
     summary << bad_rotating_service_keys.size() << " rotating auth service keys using insecure key types";
-    auto& check = next.add("AUTH_INSECURE_ROTATING_SERVICE_KEY_TYPE", HEALTH_ERR, summary.str(), bad_rotating_service_keys.size());
+    auto& check = next.add("AUTH_INSECURE_ROTATING_SERVICE_KEY_TYPE", HEALTH_WARN, summary.str(), bad_rotating_service_keys.size());
     for (auto& detail : bad_rotating_service_keys) {
       check.detail.push_back(detail);
     }