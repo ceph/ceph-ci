88cb0fb14c35a414930850aca1a5f358c31868e0
Venky Shankar <vshankar@redhat.com>
2026-08-21 17:47:13 +0530

    mgr/nfs: default to "none" access type when parsing export json
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/pybind/mgr/nfs/ganesha_conf.py
+++ src/pybind/mgr/nfs/ganesha_conf.py
@@ -471,7 +471,7 @@ class Export:
                    ex_dict.get('path', '/'),
                    ex_dict['cluster_id'],
                    ex_dict['pseudo'],
-                   ex_dict.get('access_type', 'RO'),
+                   ex_dict.get('access_type', 'none'),
                    ex_dict.get('squash', 'no_root_squash'),
                    ex_dict.get('security_label', True),
                    ex_dict.get('protocols', [4]),
